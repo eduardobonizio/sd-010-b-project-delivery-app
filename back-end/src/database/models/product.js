206d06ff38f9f290a49315dfa017b38aaaa72f8d
Mariana Savoldi <marianasavodi123@gmail.com>
2021-11-09 19:29:34 -0300

    criação do model e migration de product

---
+++ back-end/src/database/models/product.js
@@ -0,0 +1,17 @@
+'use strict';
+const Product = (sequelize, DataTypes) => {
+  const product = sequelize.define('Product', {
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL(4, 2),
+    url_image: DataTypes.STRING,
+  },
+  { timestamps: false });
+  product.associate = (models) => {
+    product.belongsTo('Sales'), {
+      foreignKey: 'seller_id',
+      as: 'sale'
+    }
+  }
+  return product;
+};
+module.exports = Product;
