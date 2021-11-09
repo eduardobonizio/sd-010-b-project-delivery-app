f5b26dd09d8868dfe7ac7d3941777136d57d93ba
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-09 20:18:26 -0300

    feat: começa a implementaçao da logica de validacao do email e senha

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,19 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [passwordIsValid, setPasswordIsValid] = useState(false);
+  const [password, setPassword] = useState('');
+  const [email, setemail] = useState('');
+  const [emailIsValid, setemailIsValid] = useState(false);
+
+  const validatePassword = (p) => {
+    const minLength = 6;
+    const isValid = p.lenght > minLength;
+    console.log('password', isValid, p);
+    setPasswordIsValid(isValid);
+  };
+
+  const savePassword = (p) => {
+    setPassword(p);
+  };
+
+  const handlePasswordChange = (event) => {
+    const { target: { value } } = event;
+    validatePassword(value);
+    savePassword(value);
+    console.log(password);
+  };
+
+  const validateEmail = (email2) => {
+    const emailRe = /\S+@\S+.\S+/;
+    const isValid = emailRe.test(email2);
+    setemailIsValid(isValid);
+  };
+
+  const saveEmail = (e) => {
+    setemail(e);
+  };
+
+  const handleEmailChange = (event) => {
+    const { target: { value } } = event;
+    validateEmail(value);
+    saveEmail(value);
+    console.log(email);
+  };
+
   return (
     <div>
       <h1>App de Delivery</h1>
       <form>
         <label htmlFor="email">
           Login
-          <input data-testid="common_login__input-email" id="email" type="email" />
+          <input
+            data-testid="common_login__input-email"
+            id="email"
+            type="email"
+            onChange={ handleEmailChange }
+          />
         </label>
         <label htmlFor="password">
           Senha
-          <input data-testid="common_login__input-password" id="password" type="email" />
+          <input
+            data-testid="common_login__input-password"
+            id="password"
+            type="email"
+            onChange={ handlePasswordChange }
+          />
         </label>
-        <button data-testid="common_login__button-login" type="button">Login</button>
+        <button
+          data-testid="common_login__button-login"
+          type="button"
+          disabled={ passwordIsValid || emailIsValid }
+        >
+          Login
+
+        </button>
         <button data-testid="common_login__button-register" type="button">
           Ainda não tenho conta
         </button>
