9d5c0ccc7081d215adc66c740b300df1695c9037
Antenor <antenorzapata@gmail.com>
2021-11-09 17:50:53 -0300

    feat: model e tabel SalesProduct

---
+++ back-end/src/database/migrations/20211109204629-create-sales-product.js
@@ -0,0 +1,32 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("salesProducts", {
+      saleId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: "sale_id",
+        references: { model: "sales", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      productId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: "product_id",
+        references: { model: "products", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("salesProducts");
+  },
+};
