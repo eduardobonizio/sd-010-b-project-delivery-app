c85923a93b33d462116c28dba838eabdc1fc9eb5
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-09 20:13:25 -0300

    Modifica o nome das chaves dos models

--- back-end/src/models/product.js
+++ back-end/src/models/product.js
@@ -2,12 +2,10 @@ const Product = (sequelize, DataTypes) => {
 const product = sequelize.define('product', {
     name: DataTypes.STRING(100),
     price: DataTypes.DECIMAL(4, 2),
-    urlImage: {
-      type: DataTypes.STRING(200),
-      field: 'url_image',
-    },
+    urlImage: DataTypes.STRING(200),
   }, {
     timestamps: false,
+    underscored: true,
   });
 
   return product;
--- back-end/src/models/sale.js
+++ back-end/src/models/sale.js
@@ -1,17 +1,17 @@
 const Sale = (sequelize, DataTypes) => {
   const sale = sequelize.define('sale', {
     totalPrice: { type: DataTypes.DECIMAL(9, 2), field: 'total_price' },
-    deliveryAddress: { type: DataTypes.STRING(100), field: 'delivery_address' },
-    deliveryNumber: { type: DataTypes.STRING(50), field: 'delivery_number' },
+    deliveryAddress: DataTypes.STRING(100),
+    deliveryNumber: DataTypes.STRING(50),
     saleDate: {
       type: DataTypes.DATETIME,
-      field: 'sale_date',
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
     status: DataTypes.STRING(50),
   }, {
     timestamps: false,
+    underscored: true,
   });
 
   return sale;
