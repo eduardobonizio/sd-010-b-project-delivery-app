2a3eca7bb9a3f7102f6b263a19b4f9445075baba
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-11-09 18:53:14 -0300

    feat: create migration products

---
+++ back-end/src/database/migrations/20211109214731-products.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("products", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DOUBLE,
+        unique: true,
+      },
+      url_image:{
+        allowNull: false,
+        type:Sequelize.STRING
+      },
+
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("products");
+  },
+};
