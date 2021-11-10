9b10fdb01af9745f81acddcc8ad36f5efb2a2820
legalvao1 <legalvao1@gmail.com>
2021-11-10 15:43:56 -0300

    Corrigindo erros de lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
 
 const app = express();
 
-/**SOURCE https://www.ti-enxame.com/pt/javascript/como-voltar-1-nivel-de-pasta-com-dirname/1052924164/ */
+/** SOURCE https://www.ti-enxame.com/pt/javascript/como-voltar-1-nivel-de-pasta-com-dirname/1052924164/ */
 app.use('/images', express.static(path.join(__dirname, '../../../', './back-end/public')));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
