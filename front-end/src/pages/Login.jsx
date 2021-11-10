465ef5086048888b45311260ebca4b3f17d137f9
Lara-Capila <laracapilak@gmail.com>
2021-11-10 20:42:33 -0300

    refactor: Refatora estrutara de pastas e rotas de acordo com requisito 1

---
+++ front-end/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '../components/Login/Form';
+
+function Login() {
+  return (
+    <main>
+      <Form />
+    </main>
+  );
+}
+
+export default Login;
--- front-end/src/routes/routes.js
+++ front-end/src/routes/routes.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Redirect } from 'react-router-dom';
+import { Route, useLocation } from 'react-router';
+
 import Form from '../components/Login/Form';
 
-const Routes = () => (
-  <Switch>
-    <Route path="/login" component={ Form } />
-  </Switch>
-);
-
+const Routes = () => {
+  const location = useLocation();
+  console.log(location.pathname);
+  return (
+    <Switch>
+      {location.pathname === '/'
+        ? <Redirect to="/login" />
+        : null}
+      <Route path="/" component={ Form } />
+    </Switch>
+  );
+};
 export default Routes;
