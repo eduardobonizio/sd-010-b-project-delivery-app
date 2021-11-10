7f8c204043fdd661a8c5fd81356e5d5139f3ed3e
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-10 15:04:42 -0300

    REFAC: User model to singular and exporting

--- back-end/src/database/models/User.js
+++ back-end/src/database/models/User.js
@@ -1,4 +1,4 @@
-const Users = (sequelize, DataTypes) => {
+module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
