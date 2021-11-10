ffcb17f5e782ed0d56a57f4baba286364d0a9473
Rafael <rmathscoach@gmail.com>
2021-11-10 18:25:23 -0300

    crio o componente EntryAddressNumber

---
+++ front-end/components/EntryAddressNumber.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Context from '../src/provider/Provider';
+
+function EntryAddressNumber() {
+  const { setAddressNumber } = React.useContext(Context);
+  return (
+    <div>
+      <label htmlFor="addressNumber">
+        Número
+        <input
+          type="number"
+          name="addressNumber"
+          data-testid="customer_checkout__input-addressNumber"
+          onChange={(e) => setAddressNumber(e.target.value)}
+        />
+      </label>
+    </div>
+  );
+}
+
+export default EntryAddressNumber;
