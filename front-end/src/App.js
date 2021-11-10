a8b1394e99d0b452ac1bde61159fff55aa07c276
andersontrkz <trkz.anderson@gmail.com>
2021-11-10 18:24:50 -0300

    adiciona layout componente

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Layout>
+      <h1>Filho</h1>
+    </Layout>
   );
 }
 
