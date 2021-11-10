a5aef5e93f523adb9d5346f5ed0ee6c250a908ab
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-10 15:24:02 -0300

    FEAT: Create Sale model and migration

---
+++ back-end/src/database/models/Sale.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const Sale = sequelize.define('Sale', {
+    user_id: { 
+      type: DataTypes.INTEGER, 
+      foreignKey: true, 
+    },
+    // TODO: Check how integrate User.role = seller to this id
+    seller_id: { 
+      type: DataTypes.INTEGER, 
+      foreignKey: true, 
+    },
+    total_price: DataTypes.DECIMAL,
+    delivery_address: DataTypes.STRING,
+    delivery_number: DataTypes.STRING,
+    sale_date: DataTypes.DATE,
+    status: DataTypes.STRING
+  },
+  { timestamps: false, tableName: 'Sales' });
+
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User, { 
+      foreignKey: 'user_id', as: 'user',
+    });
+  };
+
+  return Sale;
+};
