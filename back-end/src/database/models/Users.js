d674e368c81595599cc7f8ebf9175c9d070e4dbc
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-10 15:02:15 -0300

    FEAT: Create Users model and migration

---
+++ back-end/src/database/models/Users.js
@@ -0,0 +1,11 @@
+const Users = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
+  },
+  { timestamps: false, tableName: 'Users' });
+
+  return User;
+};
