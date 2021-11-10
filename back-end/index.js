a1540b93d77ba8f6a34eebb70799c3e0b132a7b5
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-11-10 19:29:18 -0300

    FIX: process env OR

--- back-end/index.js
+++ back-end/index.js
@@ -5,6 +5,6 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-const { API_PORT } = process.env || 3001;
+const API_PORT = process.env.API_PORT || 3001;
 
 app.listen(API_PORT, () => console.log(`ouvindo porta ${API_PORT}!`));
