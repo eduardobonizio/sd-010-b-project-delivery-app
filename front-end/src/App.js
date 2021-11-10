462f7ab5529e11e052739d95761f124498d474e7
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-11-10 19:45:51 -0300

    Criação da estrutura inicial, com os componentes e páginas que serão utilizadas, criação das rotas para renderizar as páginas

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,12 @@ import Provider from './contexts/createContext';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import './App.css';
+import Products from './pages/Products';
+import Register from './pages/Register';
+import OrderDetail from './pages/OrderDetail';
+import OrderStatus from './pages/OrderStatus';
+import Checkout from './pages/Checkout';
+import SalesDetails from './pages/SalesDetails';
 
 function App() {
   return (
@@ -12,6 +18,12 @@ function App() {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route path="/login" component={ Login } />
+          <Route path="/register" component={ Register } />
+          <Route path="/customer/products" component={ Products } />
+          <Route path="/customer/checkout" component={ Checkout } />
+          <Route path="/customer/orders/:id" component={ OrderDetail } />
+          <Route path="/customer/orders" component={ OrderStatus } />
+          <Route path="/seller/orders" component={ SalesDetails } />
           <Route path="*" render={ () => (<h1>NOT FOUND</h1>) } />
         </Switch>
       </Provider>
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
   // Uso dessa variavel somente para simular um localStorage caso n houvesse nenhum login feito
   return (
     <div>
-      { !simulationLocalStorage && <Redirect to='/login' /> }
+      { !simulationLocalStorage && <Redirect to="/login" /> }
       <h1>Home</h1>
     </div>
   );
