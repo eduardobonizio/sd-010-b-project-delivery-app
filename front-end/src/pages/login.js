8ace4cade7cad8c433064e9b28f15ef49e65992e
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-11-10 18:30:15 -0300

    form/button da pagina de login

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import rockGlass from '../images/rockGlass.svg';
 
 export default function Login() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <>
+      <form>
+
+        <label htmlFor="email">
+          Email:
+          <input type="email" name="email" data-testid="common_login__input-email" />
+        </label>
+
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            name="password"
+            data-testid="common_login__input-password"
+          />
+        </label>
+      </form>
+
+      <button
+        type="submit"
+        name="loginButton"
+        data-testid="common_login__button-login"
+      >
+        Login
+
+      </button>
+
+      <button
+        type="button"
+        name="noAccount"
+        data-testid="common_login__button-register"
+      >
+        Ainda não tenho conta
+
+      </button>
+    </>
   );
 }
