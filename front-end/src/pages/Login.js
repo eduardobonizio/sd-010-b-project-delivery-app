95eef108db8d1c007a8890e05cf8afa6d087a7e8
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-11-10 18:27:50 -0300

    Feito a pagina de Register e Login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Form from '../components/Form';
+import FormLogin from '../components/FormLogin';
 
 export default function Login() {
   return (
-    <Form />
+    <FormLogin />
   );
 }
