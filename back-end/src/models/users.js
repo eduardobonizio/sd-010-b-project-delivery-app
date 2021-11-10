787065544dcb5f873e6b88d4495feaeec7f3fc1e
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-11-10 16:59:30 -0300

    eslint fix

--- back-end/src/models/users.js
+++ back-end/src/models/users.js
@@ -2,7 +2,7 @@ const { users } = require('../database/models');
 
 const getAll = async () => {
   const allUsers = await users.findAll();
-  return allUsers 
+  return allUsers;
 };
 
 module.exports = {
