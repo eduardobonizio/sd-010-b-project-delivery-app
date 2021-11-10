71ea32e91e17c970f46ab306d00e6e8417d1e2bb
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-11-10 19:22:03 -0300

    Feat: alterando arquivos

---
+++ back-end/src/controllers/userController.js
@@ -0,0 +1,19 @@
+const userService = require('../services/userService');
+
+// const loginController = async (req, res) => {
+//   console.log('oi controller');
+//   const { body } = req;
+//   const result = await userService.loginService(body);
+//   return res.status(result.status).json(result.message);
+// };
+
+const registerController = async (req, res) => {
+  const { body } = req;
+  const newUser = await userService.createUser(body);
+  return res.status(newUser.status).json(newUser.message);
+}
+
+module.exports = { 
+  loginController,
+  registerController,
+};
