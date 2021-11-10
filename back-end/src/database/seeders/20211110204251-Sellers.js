fc93d09cd805e00e507f2927c5246dc5b124cd38
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-10 18:07:44 -0300

    FIX: Add ID to all seeders

--- back-end/src/database/seeders/20211110204251-Sellers.js
+++ back-end/src/database/seeders/20211110204251-Sellers.js
@@ -4,24 +4,28 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
      await queryInterface.bulkInsert('Users', [
     {
+      id: 2,
       name: 'Matheus',
       email: 'matheus@seller.com',
       password: '123456',
       role: 'seller'
     },
     {
+      id: 3,
       name: 'Flavio',
       email: 'flavio@seller.com',
       password: '123456',
       role: 'seller'
     },
     {
+      id: 4,
       name: 'Diego',
       email: 'diego@seller.com',
       password: '123456',
       role: 'seller'
     },
     {
+      id: 5,
       name: 'Joao',
       email: 'joao@seller.com',
       password: '123456',
