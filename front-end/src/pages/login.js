85e8acf8208dce3a2a9346a3ae8f068669bd5da4
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-11-10 18:59:01 -0300

    construçao das validaçoes de login

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,6 +1,43 @@
 import React from 'react';
+import Joi from 'joi';
+import {useValidator} from 'react-joi';
 
 export default function Login() {
+  const { state, setData, setExplicitField, validate } = useValidator({
+    initialData: {
+      email: null,
+      password: null,
+    },
+    schema: Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    }),
+    explicitCheck: {
+      email: false,
+      password: false,
+    },
+})
+
+const updateName = (e) => {
+  // react < v17
+  e.persist()
+
+  setData((old) => ({
+      ...old,
+      name: e.target.value,
+  }))
+}
+
+const updateEmail = (e) => {
+  // react < v17
+  e.persist()
+
+  setData((old) => ({
+      ...old,
+      email: e.target.value,
+  }))
+}
+
   return (
     <>
       <form>
