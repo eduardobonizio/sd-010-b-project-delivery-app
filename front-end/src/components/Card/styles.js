77773c4ee8fd69dd37ed50f245324908b8e401d2
Joao Vanelli <vanellijoao@gmail.com>
2021-11-11 16:02:21 -0300

    creating Card Component

---
+++ front-end/src/components/Card/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  height: 60vh;
+  width: 40vw;
+
+  background-color: var(--white);
+
+  border: none;
+  border-radius: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  `;
+
+export default Card;
