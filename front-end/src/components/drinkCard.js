a52d297f5f4d9cb4a93ba85171d7f79bd023f149
Leandro Reis <lleandrofr@gmail.com>
2021-11-11 12:38:36 -0300

    feat: configura data-testid para os elementos do DrinkCard

--- front-end/src/components/drinkCard.js
+++ front-end/src/components/drinkCard.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function DrinkCard({ product }) {
+  const [quantity, setQuantity] = useState(0);
+
   return (
     <div>
-      <span>{`R$ ${product.price}`}</span>
-      <img src={ product.url_image } alt={ `Foto do produto ${product.name}` } />
-      <span>{product.name}</span>
+      <span data-testid={ `customer_products__element-card-price-${product.id}` }>
+        {`R$ ${product.price}`}
+      </span>
+      <img
+        src={ product.url_image }
+        alt={ `Foto do produto ${product.name}` }
+        data-testid={ `customer_products__img-card-bg-image-${product.id}` }
+      />
+      <span data-testid={ `customer_products__element-card-title-${product.id}` }>
+        {product.name}
+      </span>
+      <button
+        type="button"
+        onClick={ () => { setQuantity(quantity - 1); } }
+        data-testid={ `customer_products__button-card-rm-item-${product.id}` }
+      >
+        -
+      </button>
+      <input
+        type="number"
+        value={ quantity }
+        data-testid={ `customer_products__input-card-quantity-${product.id}` }
+      />
+
+      <button
+        type="button"
+        onClick={ () => { setQuantity(quantity + 1); } }
+        data-testid={ `customer_products__button-card-add-item-${product.id}` }
+      >
+        +
+      </button>
     </div>
 
   );
