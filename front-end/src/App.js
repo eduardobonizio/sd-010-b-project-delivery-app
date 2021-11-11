2fdf0f9a0b3616a5a8a6deba61ef010181f612bc
Thiago Marchini <thiago@msn.com>
2021-11-11 18:27:21 -0300

    Redirecionamento de Login para Registro

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       </object>
     </div> */
     <Routes>
+      <Route path="/register" element={ <Register /> } />
       <Route path="/login" element={ <Login /> } />
       <Route path="/" element={ <Login /> } />
     </Routes>
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import { NavLink as RRNavLink, Navigate } from 'react-router-dom';
 import { setEmail, setPassword } from '../actions';
 
 function Login() {
@@ -7,6 +9,7 @@ function Login() {
   const [email, setStateEmail] = useState();
   const [password, setStatePassword] = useState();
   const [disabled, setDisable] = useState(true);
+  const [changePage, setChangePage] = useState(false);
 
   const changeDisabled = () => {
     if (email && password) {
@@ -17,26 +20,55 @@ function Login() {
   const dispatchOnSubmit = () => {
     dispatch(setEmail(email));
     dispatch(setPassword(password));
+    redirect('/customer/products');
+  };
+
+  const toRegister = () => {
+    setChangePage(true);
+    console.log(changePage);
+    return <Navigate to="/register" />;
   };
 
   return (
     <div className="login">
-      <p>LOGIN</p>
-      <input type="text" onChange={ (e) => setStateEmail(e.target.value) } />
+      <p>LOGO</p>
+      <p>NOME DO APP</p>
       <input
-        type="password"
+        data-testid="common_login__input-email"
+        onChange={ (e) => setStateEmail(e.target.value) }
+        type="text"
+      />
+      <input
+        data-testid="common_login__input-email"
         onChange={ (e) => {
           setStatePassword(e.target.value);
           changeDisabled();
         } }
+        type="password"
       />
       <button
-        type="button"
+        data-testid="common_login__button-login"
         disabled={ disabled }
         onClick={ dispatchOnSubmit }
+        type="button"
       >
-        login
+        LOGIN
       </button>
+      <Button
+        onClick={ toRegister }
+        type="button"
+        tag={ RRNavLink }
+        to="/register"
+      >
+        Ainda não tenho conta
+      </Button>
+      {/* <button
+        data-testid="common_login__button-register"
+        type="button"
+        onClick={ toRegister }
+      >
+        Ainda não tenho conta
+      </button> */}
     </div>
   );
 }
