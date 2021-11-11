a11e756b363d207527d2a1648a9911d4a4737a71
denis-rossati <denis.rossatiramos@gmail.com>
2021-11-11 18:34:43 -0300

    reforcing redirect to register button

--- front-end/src/components/RegisterComponents/RedirectToRegister.jsx
+++ front-end/src/components/RegisterComponents/RedirectToRegister.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 export default function RedirectToRegister() {
+  const [shouldRedirect, setShouldRedirect] = useState(false);
+  const redirectToRegister = () => {
+    setShouldRedirect(true);
+  };
+
   return (
-    <a
+    <button
       href="/register"
+      type="button"
       data-testid="common_login__button-register"
+      onClick={ () => redirectToRegister() }
     >
       Ainda não tenho conta
-    </a>
+      { shouldRedirect && <Redirect to="/register" />}
+    </button>
   );
 }
