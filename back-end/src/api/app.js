4f6b7d81a8fc0a1a49332fa2e13fc6095b0b7e4b
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-11-11 12:49:22 -0300

    metade do req 1

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', (_req, res) => res.redirect('login'));
+app.get('/', async (_req, res) => res.redirect('/login'));
 
-app.get('/login', (_req, res) => {
+app.get('/login', async (_req, res) => {
   res.status(200).json({ message: 'EU' });
 });
 
