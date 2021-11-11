f4d84131b274aabdfacb6ae2fff60b78ccd0461d
Joao Vanelli <vanellijoao@gmail.com>
2021-11-11 13:46:19 -0300

    fixing the structure, updating js to jsx and deleting unused files

---
+++ front-end/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+
+const App = () => (
+  <div className="App">
+    <h1>Hello World</h1>
+
+    <GlobalStyle />
+  </div>
+);
+
+export default App;
