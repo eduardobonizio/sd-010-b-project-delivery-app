9814812d56b997cee4b7d4505fed9902a82c4357
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-11-11 18:46:07 -0300

    refactor: :recycle:
    refactor endpoints using routers

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,29 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { sellerRouter } = require('../routers/sellerRouter');
+const { adminRouter } = require('../routers/adminRouter');
+const { customerRouter } = require('../routers/customerRouter');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.post('/');
 
 app.post('/login');
 
-app.post('register');
+app.post('/register');
 
-app.get('/customer/products/customer/checkout');
+/* Todas as rotas com /customer/<alguma-coisa> entram aqui e vão para o roteador */
+app.use('/customer', customerRouter);
 
-app.get('customer/orders');
+/* Todas as rotas com /seller/<alguma-coisa> entram aqui e vão para o roteador */
+app.use('/seller', sellerRouter);
 
-app.post('/customer/order/:id');
-
-app.get('/seller/orders');
-
-app.post('/seller/order/:id');
-
-app.get('/admin/manage');
-
-app.post('/admin/manage/:id');
-
-app.get('/coffee', (_req, res) => res.status(418).end());
+/* Todas as rotas com /admin/<alguma-coisa> entram aqui e vão para o roteador */
+app.use('/admin', adminRouter);
 
 module.exports = app;
