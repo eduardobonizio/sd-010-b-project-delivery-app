7afa08473af6f766b2e8f3bf643e3c7c130152ce
Daniel <dsouzaborini@gmail.com>
2021-11-11 19:17:30 -0300

    add requisito 8

--- front-end/src/pages/register.js
+++ front-end/src/pages/register.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+
+const Joi = require('joi');
 
 function Register() {
+  const [userName, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [button, setButton] = useState(true);
+  const minPasswordLength = 6;
+  const minNameLength = 12;
+
+  const registerSchema = Joi.object({
+    userName: Joi.string().min(minNameLength).required(),
+    email: Joi.string().email({ tlds: { allow: false } }).required(),
+    password: Joi.string().min(minPasswordLength).required(),
+  });
+
+  const validateLogin = () => {
+    const { error } = registerSchema.validate({ userName, email, password });
+    if (!error) setButton(false);
+    else setButton(true);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'userName') setName(value);
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+  };
+
+  useEffect(() => {
+    validateLogin();
+  }, [userName, email, password, validateLogin]);
+
   return (
     <div>
       <form>
@@ -10,10 +44,10 @@ function Register() {
             type="text"
             placeholder="Seu nome"
             id="name-input"
-            name="name"
+            name="userName"
             data-testid="common_register__input-name"
-            // onChange={ handleChange }
-            // value={ name }
+            onChange={ handleChange }
+            // value={ userName }
           />
         </label>
         <label htmlFor="email-input">
@@ -24,7 +58,7 @@ function Register() {
             id="email-input"
             name="email"
             data-testid="common_register__input-email"
-            // onChange={ handleChange }
+            onChange={ handleChange }
             // value={ email }
           />
         </label>
@@ -36,7 +70,7 @@ function Register() {
             id="password-input"
             name="password"
             data-testid="common_register__input-password"
-            // onChange={ handleChange }
+            onChange={ handleChange }
             // value={ password }
           />
         </label>
@@ -44,6 +78,7 @@ function Register() {
         <button
           type="submit"
           data-testid="common_register__button-register"
+          disabled={ button }
         >
           Cadastrar
         </button>
