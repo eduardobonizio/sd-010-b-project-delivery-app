448d5cf0106122728977dbe9bf77812909a7c630
Rafael <rmathscoach@gmail.com>
2021-11-11 16:25:45 -0300

    crio os componentes DeliveryContainer e OrderContainer

--- front-end/src/components/DeliveryContainer.jsx
+++ front-end/src/components/DeliveryContainer.jsx
@@ -1,6 +1,16 @@
-// import React from 'react';
-// import ChooseSeller from './ChooseSeller';
-// import EntryAddress from './EntryAddress';
-// import EntryAddressNumber from './EntryAddressNumber';
+import React from 'react';
+import ChooseSeller from './ChooseSeller';
+import EntryAddress from './EntryAddress';
+import EntryAddressNumber from './EntryAddressNumber';
 
+function DeliveryContainer() {
+  return (
+    <div className="delivery-container">
+      <ChooseSeller />
+      <EntryAddress />
+      <EntryAddressNumber />
+    </div>
+  );
+}
 
+export default DeliveryContainer;
