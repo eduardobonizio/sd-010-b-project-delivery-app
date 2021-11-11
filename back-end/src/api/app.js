ffd23d0cda3fca7b707773be8d1f8ddec57dfea6
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-11-11 17:00:36 -0300

    Criação de rotas, middlewares e dependências
    - Cria as rotas necessárias;
    - Cria o middleware de autenticação com JWT;
    - Instala dependência body-parser;

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.post('/');
+
+app.post('/login');
+
+app.post('register');
+
+app.get('/customer/products/customer/checkout');
+
+app.get('customer/orders');
+
+app.post('/customer/order/:id');
+
+app.get('/seller/orders');
+
+app.post('/seller/order/:id');
+
+app.get('/admin/manage');
+
+app.post('/admin/manage/:id');
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
