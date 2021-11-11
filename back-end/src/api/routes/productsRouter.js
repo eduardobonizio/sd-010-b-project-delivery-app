127a5a596223cdc4647f69ee4d8250f01ee46fcd
Matheus Rubi <matheusrubisch@gmail.com>
2021-11-11 16:19:49 -0300

    refactor: Backend - products - token

--- back-end/src/api/routes/productsRouter.js
+++ back-end/src/api/routes/productsRouter.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const rescue = require('express-rescue');
 
 const { productsController } = require('../../controllers');
+const validationToken = require('../../utils/validations/validationsToken');
 
-router.get('/', rescue(productsController.getAll));
+router.get('/', rescue(validationToken), rescue(productsController.getAll));
 
 module.exports = router;
