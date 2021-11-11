b51f700351463ea743ccb6576cf2daafcd562dd4
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-11-10 22:25:46 -0300

    TEST:testando req 1 contra o teste, aplicando as seeds padrao

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,12 +4,10 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', (_req, res) => {
-  res.redirect('login');
-});
+app.get('/', (_req, res) => res.redirect('login'));
 
 app.get('/login', (_req, res) => {
-  res.status(200).json({ message: 'OLÁ' });
+  res.status(200).json({ message: 'EU' });
 });
 
 app.get('/coffee', (_req, res) => res.status(418).end());
