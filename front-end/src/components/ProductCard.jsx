2b791b035bb72b759ced9a061e9a802c6d1e1020
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-10 21:45:14 -0300

    FEAT: Create Product's Card component

---
+++ front-end/src/components/ProductCard.jsx
@@ -0,0 +1,52 @@
+import { Container, Stack, Typography } from '@mui/material';
+import { ButtonUnstyled } from '@mui/core';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function ProductCard({ value, description, img }) {
+  const [quantity, setQuantity] = useState(0);
+
+  return (
+    // TODO: Remove inline styles
+    <Container style={ { width: '200px' } }>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={ 0.5 }
+      >
+        <img
+          style={ { width: '200px', height: '150px' } }
+          alt="imagem produto"
+          src={ img }
+        />
+        <Typography>{value}</Typography>
+        <Typography>{description}</Typography>
+        <Stack
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={ 0 }
+        >
+          <ButtonUnstyled onClick={ () => setQuantity(quantity - 1) }>-</ButtonUnstyled>
+          <input style={ { maxWidth: '15px', minWidth: '15px' } } value={ quantity } />
+          <ButtonUnstyled onClick={ () => setQuantity(quantity + 1) }>+</ButtonUnstyled>
+        </Stack>
+      </Stack>
+    </Container>
+  );
+}
+
+ProductCard.defaultProps = {
+  value: 0,
+  description: 'Descrição',
+  img: 'https://i.pinimg.com/200x150/fa/46/03/fa460302f7f057a12d1efc992e225848.jpg',
+};
+
+ProductCard.propTypes = {
+  value: PropTypes.number,
+  description: PropTypes.string,
+  img: PropTypes.string,
+};
+
+export default ProductCard;
