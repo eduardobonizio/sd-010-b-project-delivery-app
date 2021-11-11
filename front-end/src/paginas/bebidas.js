12677f3bf5c06cb5dc6f7dcdb90837d4e2c67f74
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-11 20:43:38 -0300

    removidos

--- front-end/src/paginas/bebidas.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Bebidas() {
-  return <h1>tela de bebidas</h1>;
-}
-
-export default Bebidas;
--- front-end/src/paginas/page404.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Page404() {
-  return <h1>Erro</h1>;
-}
-
-export default Page404;
