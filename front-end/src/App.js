2d6967edbefce542148f5d351953037349a816b4
Thiago Marchini <thiago@msn.com>
2021-11-11 20:44:20 -0300

    Configura o redirecionamento de / para /login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/register" element={ <Register /> } />
       <Route path="/login" element={ <Login /> } />
-      <Route path="/" element={ <Login /> } />
+      <Route path="/" element={ <Navigate to="/login" /> } />
     </Routes>
   );
 }
