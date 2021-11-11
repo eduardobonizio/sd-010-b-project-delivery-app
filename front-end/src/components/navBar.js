20fcd1d0d25685fdab6030e378508e625dd78cc1
Leandro Reis <lleandrofr@gmail.com>
2021-11-11 12:58:59 -0300

    feat: o nome do usuário é exibido na NavBar
    estas informações vem do localStorage apos o login

--- front-end/src/components/navBar.js
+++ front-end/src/components/navBar.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
+  const [userData, setUserData] = useState([]);
+
+  const updateUserData = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    setUserData(user);
+  };
+
+  useEffect(() => {
+    updateUserData();
+  }, []);
+
   return (
     <nav>
       <Link to="/customer/products">
@@ -16,7 +27,7 @@ export default function NavBar() {
       </Link>
       <Link to="/customer/products">
         <div data-testid="customer_products__element-navbar-user-full-name">
-          name_from_local_storage
+          {userData.name}
         </div>
       </Link>
       <Link to="/login">
