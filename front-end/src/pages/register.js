8dcae3ac482570a98cd61c16b6d17dae9bcfe386
Daniel <dsouzaborini@gmail.com>
2021-11-11 17:51:40 -0300

    adiciona requisito 7

--- front-end/src/pages/register.js
+++ front-end/src/pages/register.js
@@ -3,7 +3,53 @@ import React from 'react';
 function Register() {
   return (
     <div>
-      <p>Registro</p>
+      <form>
+        <label htmlFor="login-input">
+          Nome
+          <input
+            type="text"
+            placeholder="Seu nome"
+            id="name-input"
+            name="name"
+            data-testid="common_register__input-name"
+            // onChange={ handleChange }
+            // value={ name }
+          />
+        </label>
+        <label htmlFor="email-input">
+          Email
+          <input
+            type="text"
+            placeholder="email@trybeer.com"
+            id="email-input"
+            name="email"
+            data-testid="common_register__input-email"
+            // onChange={ handleChange }
+            // value={ email }
+          />
+        </label>
+        <label htmlFor="password-input">
+          Senha
+          <input
+            type="password"
+            placeholder="********"
+            id="password-input"
+            name="password"
+            data-testid="common_register__input-password"
+            // onChange={ handleChange }
+            // value={ password }
+          />
+        </label>
+
+        <button
+          type="submit"
+          data-testid="common_register__button-register"
+        >
+          Cadastrar
+        </button>
+
+        <p id="error-msg" data-testid="common_register__element-invalid_register">erro</p>
+      </form>
     </div>
   );
 }
