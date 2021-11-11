9b7f61c3d434c1e2cdcef64eb9be1731be17dad8
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-11-11 20:50:09 -0300

    error: could not find database / not resolved

--- back-end/src/database/seeders/20211109213323-users.js
+++ back-end/src/database/seeders/20211109213323-users.js
@@ -1,30 +1,30 @@
-"use strict";
-const SHA256 = require('crypto-js/sha256');
-module.exports = {
-  up: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkInsert(
-      "users",
-      [
-        {
-          id: 1,
-          name: "Lewis Hamilton",
-          email: "a@a.com",
-          password: SHA256('123456').words.join(''),
-          role: "adm",
-        },
-        {
-          id: 2,
-          name: "Michael Schumacher",
-          email: "b@b.com",
-          password: SHA256('123456').words.join(''),
-          role: "user",
-        },
-      ],
-      { timestamps: false }
-    );
-  },
+// "use strict";
+// const SHA256 = require('crypto-js/sha256');
+// module.exports = {
+//   up: async (queryInterface, _Sequelize) => {
+//     await queryInterface.bulkInsert(
+//       "users",
+//       [
+//         {
+//           id: 1,
+//           name: "Lewis Hamilton",
+//           email: "a@a.com",
+//           password: SHA256('123456').words.join(''),
+//           role: "adm",
+//         },
+//         {
+//           id: 2,
+//           name: "Michael Schumacher",
+//           email: "b@b.com",
+//           password: SHA256('123456').words.join(''),
+//           role: "user",
+//         },
+//       ],
+//       { timestamps: false }
+//     );
+//   },
 
-  down: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkDelete("users", null, {});
-  },
-};
+//   down: async (queryInterface, _Sequelize) => {
+//     await queryInterface.bulkDelete("users", null, {});
+//   },
+// };
