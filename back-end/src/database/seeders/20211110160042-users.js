ee45a4a214b6ff6bee070da26f934b927a36d002
Matheus Rubi <matheusrubisch@gmail.com>
2021-11-11 14:51:47 -0300

    refactor: Backend - Seeders

--- back-end/src/database/seeders/20211110160042-users.js
+++ back-end/src/database/seeders/20211110160042-users.js
@@ -2,9 +2,9 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('users',
       [
-        { name: 'Trybeer Admin', email: 'adm@deliveryapp.com', password: '--adm2@21!!--', role: 'administrator'},
-        { name: 'Fulana Pereira', email: 'fulana@deliveryapp.com', password: 'fulana@123', role: 'seller'},
-        { name: 'Zé Birita', email: 'zebirita@email.com', password: '$#zebirita#$', role: 'customer'},
+        { name: 'Delivery App Admin', email: 'adm@deliveryapp.com', password: 'a4c86edecc5aee06eff8fdeda69e0d04', role: 'administrator'},
+        { name: 'Fulana Pereira', email: 'fulana@deliveryapp.com', password: '3c28d2b0881bf46457a853e0b07531c6', role: 'seller'},
+        { name: 'Cliente Zé Birita', email: 'zebirita@email.com', password: '1c37466c159755ce1fa181bd247cb925', role: 'customer'},
       ],
       {},
     );
