fa0535acfc64d5e04dacee474d821c746af2b7a6
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-11 12:25:05 -0300

    build - adicionando seeders de users

--- back-end/src/database/seeders/20211111013825-users.js
+++ back-end/src/database/seeders/20211111013825-users.js
@@ -2,23 +2,27 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('users',
+    [
+      {
+        id: 1,
+        name: 'John',
+        email: 'john@gmail.com',
+        password: '123456789',
+        role: 'admin'
+      },
+      {
+        id: 2,
+        name: 'John Smith',
+        email: 'johnsmith@gmail.com',
+        password: '987654321',
+        role: 'user',
+      },
+    ],
+    { timestamps: false })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    await queryInterface.bulkDelete('users', null, {});
+  },
 };
