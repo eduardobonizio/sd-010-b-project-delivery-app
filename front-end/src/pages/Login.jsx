4d1b0819bef5226737f7bd1aa7a4ae540eb9601a
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-11-11 15:52:41 -0300

    FEAT:[LOGIN]reqs 2 & 3

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
-
 function Login() {
+  const [loginButtonDisabled, setLoginButtonDisabled] = useState(true);
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const minPasswordLength = 6;
+  const isDisabled = () => {
+    if (re.test(login) && password.length > minPasswordLength) {
+      setLoginButtonDisabled(false);
+    } else {
+      setLoginButtonDisabled(true);
+    }
+  };
+  const onChange = (value, setState) => {
+    setState(value);
+    isDisabled();
+  };
   return (
     <div>
       <form>
-        <h1>Login</h1>
+        <h1>LOGIN</h1>
         <label htmlFor="username">
-          Username:
+          Email:
           <input
-            placeholder="Digite seu username"
+            value={ login }
+            onChange={ (e) => onChange(e.target.value, setLogin) }
+            placeholder="email@tryber.com"
             data-testid="common_login__input-email"
           />
         </label>
         <label htmlFor="password">
           Senha:
           <input
+            value={ password }
+            onChange={ (e) => onChange(e.target.value, setPassword) }
             placeholder="Digite sua senha"
             data-testid="common_login__input-password"
           />
         </label>
       </form>
+      <button
+        data-testid="common_login__button-login"
+        disabled={ loginButtonDisabled }
+        type="button"
+      >
+        ENTRAR
+
+      </button>
+      <button
+        data-testid="common_login__button-register"
+        type="button"
+      >
+        Ainda não tenho conta
+      </button>
     </div>
   );
 }
