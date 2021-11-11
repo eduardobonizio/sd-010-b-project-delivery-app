fbf8fe6a5501166750565ee4c1bf65bd8d2d3258
João Victor Pistorio <joao.pistorio@gmail.com>
2021-11-11 16:21:09 -0300

    feat: Add routes and login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import Login from './pages/Login';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+
+          <Route path="/login" component={ Login } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
