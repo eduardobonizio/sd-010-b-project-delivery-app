8d9bb421921e84c118ee10372d4aeb0eb8b1a5c0
Rafael <rmathscoach@gmail.com>
2021-11-11 17:21:01 -0300

    importo os componentes e crio a página de checkout

---
+++ front-end/src/pages/checkout/Checkout.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Context from '../../provider/Provider'
+import CheckoutProducts from '../../components/CheckoutProducts'
+import PurchaseTotal from '../../components/PurchaseTotal'
+import EntryAddress from '../../components/EntryAddress';
+import PurchaseOrderBtn from '../../components/PurchaseOrderBtn';
+
+function Checkout() {
+  const {orderInProgress} = React.useContext(Context);
+  return (
+    <div>
+      {orderInProgress.map((order, index) => (
+        <CheckoutProducts
+          orderItem={order}
+          key={index}
+          index={index}
+          />
+      ))}
+      <PurchaseTotal />
+      <EntryAddress />
+      <PurchaseOrderBtn />
+    </div>
+  );
+}
+
+export default Checkout;
