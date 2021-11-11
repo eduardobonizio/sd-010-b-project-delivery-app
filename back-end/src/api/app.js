6a81231c1be60ebdd165601f2c429895f7729364
Cristian Bugs <cristian.bugs@gmail.com>
2021-11-11 16:50:26 -0300

    eslint corrected

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const router = express.Router();
+
+// const router = express.Router();
 const bodyParser = require('body-parser');
+
 const app = express();
 app.use(bodyParser.json());
 
 const login = require('../database/controller/loginController');
-
 
 app.use('/login', login);
 app.get('/coffee', (_req, res) => res.status(418).end());
