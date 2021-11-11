3287832a328b2a53e888999ae4137b8d1de897ac
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-11-11 16:06:26 -0300

    fix lint error

--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Header from './Header/Header';
+import { Header } from './Header/Header';
 
 export default function Layout({ children }) {
   return (
