2126dc18053dbf7f1597abf1a02b01c177958407
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-11-11 19:59:09 -0300

    Feat: login css

--- front-end/src/common/pages/Login.jsx
+++ front-end/src/common/pages/Login.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import '../../styles/login.css';
+
 export default function Login() {
   return (
-    <div>
-      LOGIN
-    </div>
+    <form action="">
+      <div>
+        <label htmlFor="email">
+          Login
+          <input type="text" name="email" data-testid="common_login__input-email" />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            type="password"
+            name="password"
+            data-testid="common_login__input-password"
+          />
+        </label>
+        <input type="submit" value="LOGIN" data-testid="common_login__button-login" />
+        <input
+          type="button"
+          value="Ainda não tenho conta"
+          data-testid="common_login__button-register"
+        />
+      </div>
+    </form>
   );
 }
