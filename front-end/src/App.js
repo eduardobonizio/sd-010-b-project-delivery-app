925c447d889ffaa19ebdf35b9cc80a415dfe57e8
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-11-11 00:35:17 -0300

    Feat: "/" redirects to "/login"

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import './App.css';
+import { Redirect } from 'react-router';
 import { Switch, Route } from 'react-router-dom';
+
 import Products from './common/pages/products';
 import Login from './common/pages/Login';
+
+import './App.css';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
       <Route exact path="/login" component={ Login } />
       <Route exact path="/customer/products" component={ Products } />
     </Switch>
