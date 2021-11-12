041f76b77f559d9d937e777c7af01c8385fa1eda
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-11-12 18:11:27 -0300

    test: :white_check_mark: check model name of user model

---
+++ back-end/src/tests/unit/models/userModel.test.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai');
+
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkUniqueIndex,
+  checkPropertyExists,
+} = require('sequelize-test-helpers');
+
+const UserModel = require('../../../models/user');
+
+describe('src/models/user', () => {
+  const User = UserModel(sequelize, dataTypes);
+
+  const user = new User();
+
+  checkModelName(User)('user');
+});
