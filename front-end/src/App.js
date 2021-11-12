d2c716d6f633937ee1be69f55307f6a671b0a45c
André Arnoni <andre_arnoni@yahoo.com.br>
2021-11-12 19:22:11 -0300

    Adiantado criação de páginas e rotas parte 1

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,12 @@ import Provider from './context/ContextGlobal';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import CustomerProducts from './pages/CustomerProducts';
+import CustomerCheckout from './pages/CustomerCheckout';
+import CustomerOrders from './pages/CustomerOrders';
+import CustomerOrdersId from './pages/CustomerOrdersId';
+import CustomerOrdersIdVenda from './pages/CustomerOrdersIdVenda';
+import SellerOrders from './pages/SellerOrders';
+import SellerOrdersId from './pages/SellerOrdersId';
 
 function App() {
   return (
@@ -14,6 +20,16 @@ function App() {
           <Route exact path="/login" component={ Login } />
           <Route exact path="/register" component={ Register } />
           <Route exact path="/customer/products" component={ CustomerProducts } />
+          <Route exact path="/customer/checkout" component={ CustomerCheckout } />
+          <Route exact path="/customer/orders" component={ CustomerOrders } />
+          <Route exact path="/customer/orders/:id" component={ CustomerOrdersId } />
+          <Route
+            exact
+            path="/customer/orders/:idVenda"
+            component={ CustomerOrdersIdVenda }
+          />
+          <Route path="/seller/orders" component={ SellerOrders } />
+          <Route path="/seller/orders/:id" component={ SellerOrdersId } />
         </Switch>
       </Provider>
     </BrowserRouter>
