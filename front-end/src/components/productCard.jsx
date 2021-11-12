15b6fba57f11fd00b6b7145fd9e457fe0f669f2b
Daniel <dsouzaborini@gmail.com>
2021-11-12 16:45:30 -0300

    configura botoes do product cart

---
+++ front-end/src/components/productCard.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function ProductCard(props) {
+  const [quantity, setQuantity] = useState(0);
+
+  const { product } = props;
+  const { name, price, url, id } = product;
+
+  const onClick = (e) => {
+    const { name: tagName } = e.target;
+    if (tagName === 'add') setQuantity(quantity + 1);
+    if (tagName === 'rm' && quantity > 0) setQuantity(quantity - 1);
+  };
+
+  return (
+    <div>
+      <p data-testid={ `customer_products__element-card-title-${id}` }>{ name }</p>
+      <img
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
+        src={ url }
+        alt={ name }
+      />
+      <p data-testid={ `customer_products__img-card-bg-image-${id}` }>{ price }</p>
+      <div>
+        <button
+          onClick={ onClick }
+          data-testid={ `customer_products__button-card-rm-item-${id}` }
+          type="button"
+          name="rm"
+        >
+          -
+        </button>
+        <input
+          data-testid={ `customer_products__input-card-quantity-${id}` }
+          type="text"
+          value={ quantity }
+        />
+        <button
+          onClick={ onClick }
+          data-testid={ `customer_products__button-card-add-item-${id}` }
+          type="button"
+          name="add"
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    url: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default ProductCard;
--- front-end/src/pages/products.js
+++ front-end/src/pages/products.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import Navbar from '../components/navbar';
+import ProductCard from '../components/productCard';
 
 function Products() {
+  const products = [
+    {
+      id: 1,
+      name: 'Product 1',
+      price: '$100',
+      image: 'https://picsum.photos/id/1/200/300',
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: '$200',
+      image: 'https://picsum.photos/id/2/200/300',
+    },
+  ];
+
   return (
     <div>
       <Navbar name="Camila" products="Produtos" orders="Pedidos" />
+      { products.map((product) => {
+        console.log('lint');
+        return <ProductCard key={ product.id } product={ product } />;
+      })}
     </div>
   );
 }
