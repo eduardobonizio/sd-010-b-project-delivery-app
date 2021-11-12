fb18485506df91fc852afe59cf0f22d574c5b419
André Arnoni <andre_arnoni@yahoo.com.br>
2021-11-12 18:09:04 -0300

    Trocada tela de login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { Context } from '../context/ContextGlobal';
-import LoginGif from '../images/login.gif';
+import Logo from '../images/logo.png';
 import '../styles/Login.css';
 
 function Login() {
@@ -17,52 +17,51 @@ function Login() {
 
   return (
     <div className="login__container">
+      <div className="login-heroTitle__container">
+        <h1 className="login-hero">A experiência definitiva em entrega de cerveja!</h1>
+      </div>
       <div className="login-sub__container">
-        <div>
-          <img src={ LoginGif } alt="gif delivery" className="login-image" />
-        </div>
-        <div className="login-rightSide">
-          <h1>Login</h1>
-          <form className="login-body">
-            <h4 className="login-title">Login</h4>
-            <input
-              value={ emailLogin }
-              autoComplete="off"
-              className="login-input"
-              onChange={ ({ target }) => setEmailLogin(target.value) }
-              data-testid="common_login__input-email"
-            />
-            <h4 className="login-title">Senha</h4>
-            <input
-              type="password"
-              value={ passwordLogin }
-              className="login-input"
-              onChange={ ({ target }) => setPasswordLogin(target.value) }
-              data-testid="common_login__input-password"
-            />
+        <img src={ Logo } alt="logo" className="login-logo" />
+        <form className="login-body">
+          <h4 className="login-title">Login</h4>
+          <input
+            value={ emailLogin }
+            autoComplete="off"
+            className="login-input"
+            onChange={ ({ target }) => setEmailLogin(target.value) }
+            data-testid="common_login__input-email"
+          />
+          <h4 className="login-title">Senha</h4>
+          <input
+            type="password"
+            value={ passwordLogin }
+            className="login-input"
+            onChange={ ({ target }) => setPasswordLogin(target.value) }
+            data-testid="common_login__input-password"
+          />
+          <Button
+            className="login-button1"
+            variant="success"
+            type="submit"
+            data-testid="common_login__button-login"
+            disabled={
+              !(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(emailLogin)
+            || passwordLogin.length <= passLength
+            }
+          >
+            LOGIN
+          </Button>
+          <Link to="/register">
             <Button
-              className="login-button"
-              type="submit"
-              data-testid="common_login__button-login"
-              disabled={
-                !(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(emailLogin)
-              || passwordLogin.length <= passLength
-              }
+              className="login-button2"
+              data-testid="common_login__button-register"
             >
-              LOGIN
+              Ainda não tenho conta
             </Button>
-            <Link to="/register">
-              <Button
-                className="login-button"
-                data-testid="common_login__button-register"
-              >
-                Ainda não tenho conta
-              </Button>
-            </Link>
-          </form>
-          {errorLogin
-            && <span data-testid="common_login__element-invalid-email">{ error }</span>}
-        </div> 
+          </Link>
+        </form>
+        {errorLogin
+          && <span data-testid="common_login__element-invalid-email">{ error }</span>}
       </div>
     </div>
   );
