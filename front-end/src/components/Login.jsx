056eddfcf50a60ebef45cbc69c132419acf6d432
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-11-12 18:40:55 -0300

    fix importação errada do useState

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react/cjs/react.development';
 
 import DeliveryContext from '../context/DeliveryContext';
 
