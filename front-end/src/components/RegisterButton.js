6bacc951af3ae246e4eb16121ed8bde1c7f3cdef
Nícolas Tozato <ntozato9@gmail.com>
2021-11-12 17:46:00 -0300

    fix-emailRegex

--- front-end/src/components/RegisterButton.js
+++ front-end/src/components/RegisterButton.js
@@ -5,7 +5,7 @@ const RegisterButton = () => {
   const { registerData } = useContext(Context);
   const nameMinLength = 12;
   const passwordMinLength = 6;
-  const emailRegex = /\S+@\S+\.\S+/;
+  const emailRegex = /^[_.a-z0-9-]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
   const isEmailValid = emailRegex.test(registerData.email);
   const isValidName = registerData.name.length >= nameMinLength;
   const isValidPassword = registerData.password.length >= passwordMinLength;
