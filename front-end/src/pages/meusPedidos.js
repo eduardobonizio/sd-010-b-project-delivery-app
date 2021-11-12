5ab1ab428475b0472a9d974a76c10525e0d586c8
Diegho <dieghonm@gmail.com>
2021-11-12 18:33:01 -0300

    Navbar com rotas

---
+++ front-end/src/pages/meusPedidos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './component/navbar';
+
+function MeusPedidos() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Meus Pedidos</h1>
+    </div>
+  );
+}
+
+export default MeusPedidos;
