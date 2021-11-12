1cac3b86b2e88d9a355b5ebee1702d54813e2b89
EderPaiva <edergomesdepaiva12@gmail.com>
2021-11-12 20:24:50 -0300

    Feat: Adicionando elementos da tela de cadastro

--- front-end/src/pages/Common/Register/index.jsx
+++ front-end/src/pages/Common/Register/index.jsx
@@ -2,16 +2,40 @@ import React from 'react';
 
 export default function Register() {
   return (
-    <div>
-      <div>
-        <input />
+    <>
+      <h1>Cadastro</h1>
+      <div display="row">
+        <label htmlFor="name">
+          Nome
+          <input
+            id="name"
+            data-testid="common_register__input-name"
+            placeholder="Seu nome"
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            data-testid="common_register__input-email"
+            placeholder="seu-email@site.com.br"
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            data-testid="common_register__input-password"
+            placeholder="******"
+          />
+        </label>
+        <button
+          data-testid="common_register__button-register"
+          type="submit"
+        >
+          CADASTRAR
+        </button>
+        <p>{' '}</p>
       </div>
-      <div>
-        <input />
-      </div>
-      <div>
-        <input />
-      </div>
-    </div>
+    </>
   );
 }
