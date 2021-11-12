d0c677d9ea32ca157995bb12cd5956a1f5e48b06
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-11-12 17:06:05 -0300

    fix evaluator

--- back-end/src/api/Controllers/loginController.js
+++ back-end/src/api/Controllers/loginController.js
@@ -2,13 +2,15 @@ const userService = require('../Services/loginService');
 
 const loginUser = async (req, res) => {
   const { body } = req;
-
-  const login = await userService.loginUser(body);
-  if (login.message) {
-    return res.status(404).json(login.message);
+  try {
+    const login = await userService.loginUser(body);
+    if (login.message) {
+      return res.status(404).json(login.message);
+    }
+    return res.status(200).json({ token: login });
+  } catch (error) {
+    return res.status(500).json(error);
   }
-
-  return res.status(200).json({ token: login });
 };
 
 module.exports = {
