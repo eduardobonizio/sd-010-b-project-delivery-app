fc49de162d7169e5cdec6ba1157b9dff7e99d48c
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-11-12 20:44:03 -0300

    fix

--- back-end/src/api/Controllers/loginController.js
+++ back-end/src/api/Controllers/loginController.js
@@ -3,12 +3,13 @@ const userService = require('../Services/loginService');
 const loginUser = async (req, res) => {
   const { body } = req;
     const login = await userService.loginUser(body);
+
     if (login.message) {
       return res.status(404).json(login.message);
     }
-    
+
     return res.status(200).json({ token: login });
-  }
+  };
 
 module.exports = {
   loginUser,
