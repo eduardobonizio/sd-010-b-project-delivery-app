5b88e78f807f341199d36817506cfcb42b7fd77f
Caio <caioqs.info@gmail.com>
2021-11-12 19:44:47 -0300

    Feito css do componente Cards e criado data-testid

--- front-end/src/components/Cards.jsx
+++ front-end/src/components/Cards.jsx
@@ -1,23 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { GoPlus, GoDash } from 'react-icons/go';
 import '../styles/Card.css';
 
-function Cards() {
+function Cards({ values }) {
+  const { id, name, price, urlImage } = values;
   return (
     <div className="div-cards">
       <div className="div-image">
-        <image alt="Cerveja" />
+        <div
+          className="div-price"
+          data-testid={ `customer_products__element-card-price-${id}` }
+        >
+          {`R$ ${price},00`}
+        </div>
+        <img
+          alt="Cerveja"
+          src={ urlImage }
+          data-testid={ `customer_products__img-card-bg-image-${id}` }
+        />
       </div>
       <div className="div-p-buttons">
         <div className="div-p">
-          <p>Cerveja</p>
+          <p data-testid={ `customer_products__element-card-title-${id}` }>{ name }</p>
         </div>
         <div className="div-buttons">
-          <button type="button">
+          <button
+            type="button"
+            data-testid={ `customer_products__button-card-rm-item-${id}` }
+          >
             <GoDash aria-label="sub" style={ { color: 'white' } } />
           </button>
-          <div className="quantity">0</div>
-          <button type="button">
+          <div
+            className="quantity"
+            data-testid={ `customer_products__input-card-quantity-${id}` }
+          >
+            0
+          </div>
+          <button
+            type="button"
+            data-testid={ `customer_products__button-card-add-item-${id}` }
+          >
             <GoPlus aria-label="somar" style={ { color: 'white' } } />
           </button>
         </div>
@@ -25,5 +48,9 @@ function Cards() {
     </div>
   );
 }
+
+Cards.propTypes = {
+  values: PropTypes.object,
+}.isRequired;
 
 export default Cards;
--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from '../components/Header';
 import Cards from '../components/Cards';
 import '../styles/Card.css';
 
 function Products() {
+  const [products, setProducts] = useState([]);
+  console.log(products);
+  useEffect(() => {
+    axios.get('http://localhost:3001/products').then((res) => {
+      const resp = res.data;
+      setProducts(resp);
+    });
+  }, []);
+
   return (
     <div className="div-main">
       <Header />
       <main>
-        <Cards />
+        {products.map((product) => <Cards key={ product.id } values={ product } />)}
       </main>
     </div>
   );
