0e60a94a5a8e1240829f98db09c3d36e615f989f
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-12 20:14:14 -0300

    declarado path para images no back

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use('/images', express.static(path.resolve(__dirname, '../../public/images')))
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
