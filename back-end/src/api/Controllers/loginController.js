cfce918ebc96e5de80f64d0ea1a15a8f7f32107b
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-11-12 19:37:12 -0300

    retirado try cath

--- back-end/src/api/Controllers/loginController.js
+++ back-end/src/api/Controllers/loginController.js
@@ -2,16 +2,14 @@ const userService = require('../Services/loginService');
 
 const loginUser = async (req, res) => {
   const { body } = req;
-  try {
     const login = await userService.loginUser(body);
+
     if (login.message) {
       return res.status(404).json(login.message);
     }
+    
     return res.status(200).json({ token: login });
-  } catch (error) {
-    return res.status(500).json(error);
   }
-};
 
 module.exports = {
   loginUser,
