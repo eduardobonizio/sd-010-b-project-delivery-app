673789279fec366ffcf513c8b73012cfc0707dbe
Pedro Toselli <phtoselli@gmail.com>
2021-11-12 00:36:57 -0300

    Style: criando os Styled Components para a página login

--- front-end/src/pages/Common/Login/index.jsx
+++ front-end/src/pages/Common/Login/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import * as style from './style';
 
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <style.CommonContainer>
+
+      <style.LoginContainer />
+
+      <style.CommonForm>
+        <style.InputLabel>
+          Login
+          <br />
+          <style.InputEmail data-testid="common_login__input-email" />
+        </style.InputLabel>
+        <style.InputLabel>
+          Senha
+          <br />
+          <style.InputPassword data-testid="common_login__input-password" />
+        </style.InputLabel>
+        <style.LoginButton type="submit">Login</style.LoginButton>
+        <Link to="/register">
+          <style.RegisterButton type="submit">Ainda nao tenho conta</style.RegisterButton>
+        </Link>
+      </style.CommonForm>
+    </style.CommonContainer>
   );
 }
