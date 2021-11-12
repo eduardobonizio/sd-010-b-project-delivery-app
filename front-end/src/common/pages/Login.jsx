9c50e5440f5c4d83f8f5a32dde50260134ee86a9
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-11-12 00:07:09 -0300

    Feat: login screen like the prototype

--- front-end/src/common/pages/Login.jsx
+++ front-end/src/common/pages/Login.jsx
@@ -4,27 +4,41 @@ import '../../styles/login.css';
 
 export default function Login() {
   return (
-    <form action="">
-      <div>
-        <label htmlFor="email">
-          Login
-          <input type="text" name="email" data-testid="common_login__input-email" />
-        </label>
-        <label htmlFor="password">
-          Senha
-          <input
-            type="password"
-            name="password"
-            data-testid="common_login__input-password"
-          />
-        </label>
-        <input type="submit" value="LOGIN" data-testid="common_login__button-login" />
-        <input
-          type="button"
-          value="Ainda não tenho conta"
-          data-testid="common_login__button-register"
-        />
-      </div>
-    </form>
+    <div className="login-content">
+      <form action="">
+        <div>
+          <label htmlFor="email">
+            Login
+            <input type="text" name="email" data-testid="common_login__input-email" />
+          </label>
+          <label htmlFor="password">
+            Senha
+            <input
+              type="password"
+              name="password"
+              data-testid="common_login__input-password"
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="common_login__button-login"
+          >
+            LOGIN
+          </button>
+          <button
+            type="button"
+            data-testid="common_login__button-register"
+          >
+            Ainda não tenho conta
+          </button>
+        </div>
+      </form>
+      <span
+        data-testid="common_login__element-invalid-email"
+        hidden
+      >
+        Erro!
+      </span>
+    </div>
   );
 }
