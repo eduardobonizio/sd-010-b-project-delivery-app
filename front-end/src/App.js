a402cfebe2bd31cee331a1223d2a42a00ef78f56
Jessie <miss_sc_jessi@hotmail.com>
2021-11-12 15:07:59 -0300

    excluindo comentarios, tela Home comentada

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,21 +2,13 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login';
-// import Home from './Components/Home';
 
 function App() {
-  // const location = useLocation();
-
   return (
     <div className="App">
       <Switch>
         <Route path="/login" component={ Login } />
-        <Route
-          exact
-          path="/"
-          // element={ <Home /> }
-          // render={ (props) => <Home { ...props } /> }
-        >
+        <Route exact path="/">
           <Redirect to="/login" />
         </Route>
       </Switch>
