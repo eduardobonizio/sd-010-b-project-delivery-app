9e8243a4b418830f45420192423a2fd26db22c19
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-11-12 12:58:33 -0300

    fix:  changed seeds

--- back-end/src/database/seeders/20211109213323-users.js
+++ back-end/src/database/seeders/20211109213323-users.js
@@ -7,17 +7,24 @@ module.exports = {
       [
         {
           id: 1,
-          name: "Lewis Hamilton",
-          email: "a@a.com",
-          password: SHA256('123456').words.join(''),
-          role: "adm",
+          name: "Delivery App Admin",
+          email: "adm@deliveryapp.com",
+          password: "a4c86edecc5aee06eff8fdeda69e0d04", // //senha: md5('--adm2@21!!--')
+          role: "administrator",
         },
         {
           id: 2,
-          name: "Michael Schumacher",
-          email: "b@b.com",
-          password: SHA256('123456').words.join(''),
-          role: "user",
+          name: "Fulana Pereira",
+          email: "fulana@deliveryapp.com",
+          password: '3c28d2b0881bf46457a853e0b07531c6', //// senha: md5('fulana@123')
+          role: "seller",
+        },
+        {
+          id: 3,
+          name: "Cliente Zé Birita",
+          email: "zebirita@email.com",
+          password: '1c37466c159755ce1fa181bd247cb925', //  //md5('$#zebirita#$')
+          role: "customer",
         },
       ],
       { timestamps: false }
