e5611626f066ac2226046dc0709ed0c4bd3f01ef
Leandro Reis <lleandrofr@gmail.com>
2021-11-13 12:48:33 -0300

    feat: ao finalizar pedido é feita uma req POST ao database
    Ao clicar no botao de finalizar pedido, é feita uma requisiçao POST no
    endpoint /sales para cadastro da venda no banco de dados. O token de
    autenticação é enviado no header da requisição

--- front-end/src/components/deliveryDetails.js
+++ front-end/src/components/deliveryDetails.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import ProductsContext from '../context/productContext';
 import { fetchAllSellers } from '../services/userAPI';
+import postSale from '../services/saleAPI';
 
 const initialState = {
-  seller: '',
+  seller: 1,
   address: '',
   number: 0,
 };
@@ -11,8 +15,11 @@ export default function DeliveryDetails() {
   const [checkoutInfo, setCheckoutInfo] = useState(initialState);
   const [sellers, setSellers] = useState([]);
 
+  const history = useHistory();
+  const { totalPrice } = useContext(ProductsContext);
+
   const updateState = (name, value) => {
-    setCheckoutInfo({ ...inputData, [name]: value });
+    setCheckoutInfo({ ...checkoutInfo, [name]: value });
   };
 
   useEffect(() => {
@@ -23,8 +30,22 @@ export default function DeliveryDetails() {
     getSellers();
   }, []);
 
-  // TODO: remover o console.log abaixo após configurar checkout
-  console.log(checkoutInfo);
+  const finishSale = async () => {
+    const { address, number, seller } = checkoutInfo;
+    const { id, token } = JSON.parse(localStorage.getItem('user'));
+
+    const payload = {
+      user_id: id,
+      seller_id: seller,
+      total_price: totalPrice.toFixed(2),
+      delivery_address: address,
+      delivery_number: number,
+    };
+
+    const { id: saleId } = await postSale(payload, token);
+
+    history.push(`/customer/orders/${saleId}`);
+  };
 
   return (
     <form>
@@ -35,12 +56,16 @@ export default function DeliveryDetails() {
           id="seller"
           name="seller"
           type="text"
+          required
           data-testid="customer_checkout__select-seller"
-          onChange={ ({ target: t }) => { updateState(t.name, t.value); } }
+          onClick={ ({ target: t }) => { updateState(t.name, t.value); } }
         >
-          {sellers.map(({ name }) => (
-            <option key={ name }>{name}</option>
-          ))}
+          {sellers.map(({ name, id }, idx) => {
+            if (idx === 0) {
+              return <option key={ name } defaultValue value={ id }>{name}</option>;
+            }
+            return <option key={ name } value={ id }>{name}</option>;
+          })}
         </select>
       </label>
       <label htmlFor="address">
@@ -49,6 +74,7 @@ export default function DeliveryDetails() {
           id="address"
           name="address"
           type="text"
+          required
           data-testid="customer_checkout__input-address"
           onChange={ ({ target: t }) => { updateState(t.name, t.value); } }
         />
@@ -60,15 +86,16 @@ export default function DeliveryDetails() {
           name="number"
           type="number"
           data-testid="customer_checkout__input-addressNumber"
+          required
           onChange={ ({ target: t }) => { updateState(t.name, t.value); } }
         />
       </label>
       <button
         type="button"
         data-testid="customer_checkout__button-submit-order"
+        onClick={ () => { finishSale(); } }
       >
         Finalizar Pedido
-
       </button>
     </form>
   );
