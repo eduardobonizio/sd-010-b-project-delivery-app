9edf7c2bedabfe09fece68ca2ea4ce9fe4728b97
Leandro Reis <lleandrofr@gmail.com>
2021-11-13 13:42:55 -0300

    feat: atualiza a tabela salesProducts ao realizar cadastrar uma venda

--- back-end/src/api/services/saleService.js
+++ back-end/src/api/services/saleService.js
@@ -1,8 +1,8 @@
-const { sale } = require('../../database/models');
+const { sale, salesProducts } = require('../../database/models');
 
 const createSale = async (body) => {
-  const { user_id, seller_id, total_price, delivery_address, delivery_number } = body;
-  return sale.create(
+  const { user_id, seller_id, total_price, delivery_address, delivery_number, products } = body;
+  const newSale = await sale.create(
     // eslint desabilitado temporariamente para testes
     {
       user_id,    // eslint-disable-line
@@ -14,6 +14,13 @@ const createSale = async (body) => {
       status: 'Pendente',
     },
   );
+  
+  products.forEach(async ({ id, quantity }) => {
+    // eslint-disable-next-line
+    await salesProducts.create({ sale_id: newSale.id, product_id: id, quantity });
+  });
+
+  return newSale;
 };
 
 module.exports = {
