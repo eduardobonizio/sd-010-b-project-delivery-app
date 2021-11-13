7ff72e3135ae50240ce7bc1072db8f502b8cbb04
andersontrkz <trkz.anderson@gmail.com>
2021-11-13 01:23:32 -0300

    inicia requisito 16

--- front-end/src/components/CartCard.jsx
+++ front-end/src/components/CartCard.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router';
 
 import Context from '../context/Context';
 
@@ -6,17 +7,23 @@ import '../styles/cartCard.css';
 
 const CartCard = () => {
   const { totalCart } = useContext(Context);
+  const history = useHistory();
 
   const convertDotToComma = (value) => value.toString().replace('.', ',');
 
   return (
-    <span className="cart-card__container">
+    <button
+      type="button"
+      className="cart-card__container"
+      data-testid="customer_products__button-cart"
+      onClick={ () => history.push('/customer/checkout') }
+    >
       Ver Carrinho R$
       {' '}
       <strong data-testid="customer_products__checkout-bottom-value">
         { convertDotToComma(totalCart) }
       </strong>
-    </span>
+    </button>
   );
 };
 
