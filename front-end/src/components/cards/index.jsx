ebef95d36599ebb507201e603e5fa70f3af8eb21
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-14 20:08:54 -0300

    Feat: Função que controla input e botão

--- front-end/src/components/cards/index.jsx
+++ front-end/src/components/cards/index.jsx
@@ -20,6 +20,50 @@ import {
 // import { setOnLocalStorage } from '../../helpers/localStorage';
 
 function CardsProducts() {
+  const [dataProducts, setDataProducts] = useState([]);
+  const [inputQuantity, setInputQuantity] = useState([]);
+
+  const valueTotalProduct = dataProducts
+    .reduce((acc, curr) => acc + (curr.price * curr.quantity), 0);
+
+  console.log(inputQuantity);
+  useEffect(() => {
+    async function apiRequest() {
+      const response = await apiGetAllProducts();
+      const newResponse = response.map((value) => ({
+        ...value,
+        quantity: 0,
+      }));
+      const valueQuantity = newResponse.map((value) => ({
+        name: value.name,
+        quantity: Number(value.quantity),
+        price: value.price,
+      }));
+      setDataProducts(newResponse);
+      setInputQuantity(valueQuantity);
+    }
+    apiRequest();
+  }, []);
+
+  function handleOnClick(index, event) {
+    const { id } = event.target;
+    const valuesDataProducts = [...dataProducts];
+    if (id === 'add') {
+      valuesDataProducts[index][event.target.name] += 1;
+      setDataProducts(valuesDataProducts);
+    }
+    if (id === 'rm' && valuesDataProducts[index][event.target.name] > 0) {
+      valuesDataProducts[index][event.target.name] -= 1;
+      setDataProducts(valuesDataProducts);
+    }
+  }
+
+  function handleOnChange(index, event) {
+    const valuesDataProducts = [...dataProducts];
+    valuesDataProducts[index][event.target.name] = Number(event.target.value);
+    setDataProducts(valuesDataProducts);
+  }
+
   return (
     <ContainerListCard>
       <ContainerCard>
