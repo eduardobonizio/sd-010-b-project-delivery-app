9fe0f06af7d61e0059422eeafafc38b4d597f68d
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-14 20:08:06 -0300

    build: Import bibliotecas e compomentes

--- front-end/src/components/cards/index.jsx
+++ front-end/src/components/cards/index.jsx
@@ -1,5 +1,23 @@
-import React from 'react';
-import { ContainerCard, ContainerListCard, ContainerQuantity } from './styles';
+/* eslint-disable react/jsx-no-bind */
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import apiGetAllProducts from '../../services/products/customerProduct';
+import imageCerveja from '../../images/longs.jpg';
+import {
+  ButtonCart,
+  ButtonQuantitySubtract,
+  ButtonQuantitySum,
+  ContainerCard,
+  ContainerInput,
+  ContainerListCard,
+  ContainerQuantity,
+  CotainerPrice,
+  ImageProduct,
+  InputQuantity,
+  TextName,
+  TextPrice,
+} from './styles';
+// import { setOnLocalStorage } from '../../helpers/localStorage';
 
 function CardsProducts() {
   return (
