e50f4d0f2740d997e7c4a5670c540682157023a1
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-15 04:21:14 -0300

    REDO: Creates 2 seeders

---
+++ back-end/src/database/seeders/20211115070328-SalesProducts.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.bulkInsert('salesProducts', [
+    {
+      sale_id: 1,
+      product_id: 1,
+      quantity: 1,
+    },
+    {
+      sale_id: 2,
+      product_id: 2,
+      quantity: 1,
+    },
+    {
+      sale_id: 3,
+      product_id: 3,
+      quantity: 1,
+    },
+    {
+      sale_id: 4,
+      product_id: 4,
+      quantity: 1,
+    },
+    ], {});
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('salesProducts', null, {}); 
+  }
+};
