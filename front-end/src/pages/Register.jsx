5705643d7bc4b843428138e3602982b41a874a68
Lara-Capila <laracapilak@gmail.com>
2021-11-16 19:41:33 -0300

    feat: Adiciona título na tela de cadastro

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Typography } from 'antd';
+
 import FormRegister from '../components/Register/FormRegister';
 
 function Register() {
+  const { Title } = Typography;
   return (
     <main>
+      <Title
+        style={ { textAlign: 'center', margin: '50px 0 0 0' } }
+      >
+        Cadastro
+      </Title>
       <FormRegister />
     </main>
   );
