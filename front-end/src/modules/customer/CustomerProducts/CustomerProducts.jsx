e61078ee3a8309b8dc67df5d33020f0d2356b85d
Paulo Xavier <pxavierf@gmail.com>
2021-11-16 18:07:24 -0300

    push

---
+++ front-end/src/modules/customer/CustomerProducts/CustomerProducts.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ProductList from './components/ProductList';
+import RedirectButton from '../../../components/RedirectButton';
+import Navbar from '../../../components/Navbar';
+
+function CustomerProducts() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return (
+    <div>
+      <Navbar
+        item1="PRODUTOS"
+        item2="MEUS PEDIDOS"
+        user={ user.name }
+      />
+      <ProductList />
+      <RedirectButton />
+    </div>
+  );
+}
+
+export default CustomerProducts;
---
+++ front-end/src/modules/customer/CustomerProducts/components/ProductList.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import Cards from '../../../../components/Cards';
+import api from '../../../../services/api';
+
+function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getAllProducts() {
+      const response = await api.get('/products');
+      setProducts(response.data);
+    }
+    getAllProducts();
+  }, []);
+  console.log(products);
+  return (
+    <div>
+      {products.map((product) => (
+        <Cards key={ product.id } product={ product } />
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
