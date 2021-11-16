96d498349150d3511b94cbdcf126f7a6783b9fca
Pedro Toselli <phtoselli@gmail.com>
2021-11-16 19:41:05 -0300

    Build: renomeando função e colocando o array 'sales' para receber o conteudo do banco de dados

--- front-end/src/pages/Client/MyRequests/index.jsx
+++ front-end/src/pages/Client/MyRequests/index.jsx
@@ -1,58 +1,81 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import { apiGetAllSales } from '../../../services/salesProducts/apiRequestSalesProduct';
 import Header from '../../../components/navbar';
 import * as style from './style';
-import { apiGetAllSales } from '../../../services/salesProducts/apiRequestSalesProduct';
-/* eslint-disable */
+/* eslint-disable */  // excluir essa linha quando for enviar o requisito para merge ( essa linha desabilita o eslint para que ele nao quebre o projeto)
 
-export default function ClientOrderDetails() {
-  const [Array, setArray] = useState([
-    { pedido: 1, status: 'pendente', data: '01/01/0000', valor: 1000 },
-    { pedido: 2, status: 'entregue', data: '02/02/0000', valor: 1020 },
-    { pedido: 3, status: 'preparando', data: '03/03/0000', valor: 1040 },
+export default function CustomerSales() {
+  // sales é o array que vem do banco de dados
+  const [sales, setSales] = useState([
+    { pedido: 1, status: 'pendente', data: '01/01/0000', valor: 1000 }, // apagar esse objeto quando o GET tiver dado certo
   ]);
 
-  const [sales, setSales] = useState([]);
-
+  // esse useEffect faz um GET de todas as compras do usuario e coloca dentro de "sales" com o setSales;
   useEffect(() => {
     const data = apiGetAllSales()
       .then((res) => res)
-      .catch((err) => console.log('Erro:', err));
+      .catch((err) => console.log(err));
 
-    setSales(data);
-  }, [])
+    // setSales(data);  // essa linha serve para enviar ao "sales" o que vem do nosso GET (envia um array de objetos)
+  }, []);
 
+  // esse useEffect serve apenas para vizualizar o que o GET está colocando dentro do nosso array "sales"
   useEffect(() => {
     console.log(sales);
-  }, [sales])
+  }, [sales]);
 
-  // trocar o index pelo numero do pedido
+  /*
+
+  O QUE PRECISA SER FEITO:
+
+  - trocar o index pelo numero do pedido
+  - trocar "curr.pedido", "curr.status", "curr.data", etc... pelo nome que vem do banco de dados ex: "curr.sale_id"
+  - remover o objeto de dentro de "sales"
+  - fazer a integração do frontend com o backend
+  - caso necessário, trocar a tag "p" por uma tag específica correspondente
+
+  OBSERVAÇÕES:
+
+  - caso precise mudar algo relacionado ao css, favo utilizar styledComponent para isso.
+
+  */
 
   return (
     <>
       <Header />
       <style.DetailsContainer>
         {
-          Array.map((curr, index) => {
-            return (
-              <Link key={`link:${index}`} to={ `${index}` }>
-                <style.DetailsCard key={`card:${index}`}>
-                  <p key={ `pedido:${index}` } data-testid={ `customer_orders__element-order-id-${index}` }>
-                    { curr.pedido }
-                  </p>
-                  <p key={ `status:${index}` } data-testid={ `customer_orders__element-delivery-status-${index}` }>
-                    { curr.status }
-                  </p>
-                  <p key={ `data:${index}` } data-testid={ `customer_orders__element-order-date-${index}` }>
-                    { curr.data }
-                  </p>
-                  <p key={ `valor:${index}` }>
-                    { curr.valor }
-                  </p>
-                </style.DetailsCard>
-              </Link>
-            )
-          })
+          sales.map((curr, index) => (
+            <Link key={ `link:${index}` } to={ `${index}` }>
+              <style.DetailsCard key={ `card:${index}` }>
+                <p
+                  key={ `pedido:${index}` }
+                  data-testid={ `customer_orders__element-order-id-${index}` }
+                >
+                  { curr.pedido }
+                </p>
+                <p
+                  key={ `status:${index}` }
+                  data-testid={ `customer_orders__element-delivery-status-${index}` }
+                >
+                  { curr.status }
+                </p>
+                <p
+                  key={ `data:${index}` }
+                  data-testid={ `customer_orders__element-order-date-${index}` }
+                >
+                  { curr.data }
+                </p>
+                <p
+                  key={ `valor:${index}` }
+                >
+                  { curr.valor }
+                </p>
+              </style.DetailsCard>
+            </Link>
+          ))
         }
       </style.DetailsContainer>
     </>
