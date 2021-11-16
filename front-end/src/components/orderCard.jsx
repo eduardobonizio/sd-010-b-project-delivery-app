faec68a15e7febd8fb9bc5f8a0a355db04370284
Daniel <dsouzaborini@gmail.com>
2021-11-16 16:29:54 -0300

    add test id

--- front-end/src/components/orderCard.jsx
+++ front-end/src/components/orderCard.jsx
@@ -2,18 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function OrderCart(props) {
-  const { orderNum, orderDate, orderTotal, orderStatus } = props;
+  const { order } = props;
+  const { id: orderNum, orderDate, orderTotal, orderStatus } = order;
 
   return (
     <div>
       <div>
-        <h3>{`Pedido ${orderNum}`}</h3>
+        <h3
+          data-testid={ `customer_orders__element-order-id-${orderNum}` }
+        >
+          {`Pedido ${orderNum}`}
+        </h3>
       </div>
       <div>
-        <h3>{orderStatus}</h3>
+        <h3
+          data-testid={ `customer_orders__element-delivery-status-${orderNum}` }
+        >
+          {orderStatus}
+        </h3>
       </div>
       <div>
-        <p>{orderDate}</p>
+        <p
+          data-testid={ `customer_orders__element-order-date-${orderNum}` }
+        >
+          {orderDate}
+        </p>
         <p>{orderTotal}</p>
       </div>
     </div>
@@ -21,10 +34,13 @@ function OrderCart(props) {
 }
 
 OrderCart.propTypes = {
-  orderNum: PropTypes.number.isRequired,
-  orderDate: PropTypes.string.isRequired,
-  orderTotal: PropTypes.string.isRequired,
-  orderStatus: PropTypes.string.isRequired,
+  order: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    orderNum: PropTypes.number.isRequired,
+    orderDate: PropTypes.string.isRequired,
+    orderTotal: PropTypes.string.isRequired,
+    orderStatus: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default OrderCart;
