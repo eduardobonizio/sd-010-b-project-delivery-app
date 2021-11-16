9adec65a278fd0fc1298932e97ccaf47ac068c8d
Jessie <miss_sc_jessi@hotmail.com>
2021-11-16 20:20:11 -0300

    verifica existencia do usuario pelo nome e o email e cria o usuario

--- back-end/src/controllers/users.js
+++ back-end/src/controllers/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const rescue = require('express-rescue');
-const { getUserByEmail } = require('../services/users');
+const { getUserByEmail,
+  getUserByName,
+  createUser,
+  } = require('../services/users');
 
 const userRouter = express.Router();
 
@@ -11,5 +14,16 @@ userRouter.post('/', rescue(async (req, res) => {
   if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
   return res.status(200).json(user);
 }));
+
+userRouter.post('/register', rescue(async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const findUserName = await getUserByName(name);
+  const findUserEmail = await getUserByEmail(email);
+  if (findUserName || findUserEmail) return res.status(409).json({ message: 'Usuário já existe' });
+
+  const create = await createUser(name, email, password, role);
+  return res.status(201).json(create);
+}))
 
 module.exports = { userRouter };
--- back-end/src/services/users.js
+++ back-end/src/services/users.js
@@ -5,6 +5,18 @@ const getUserByEmail = async (email) => {
   return user;
 };
 
+const getUserByName = async (name) => {
+  const user = await User.findOne({ where: { name } });
+  return user;
+}
+
+const createUser = async (name, email, password, role) => {
+  const user = await User.create({ name, email, password, role });
+  return user;
+};
+
 module.exports = {
  getUserByEmail,
+ getUserByName,
+ createUser,
 };
