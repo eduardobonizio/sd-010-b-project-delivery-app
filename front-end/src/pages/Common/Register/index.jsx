37c4cfb6fe131a3cbfeecbeacdb2d748cbaa5e33
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-16 13:44:24 -0300

    fix - finalizando requisito 8

--- front-end/src/pages/Common/Register/index.jsx
+++ front-end/src/pages/Common/Register/index.jsx
@@ -1,21 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Register() {
-  const [btnDisable] = useState(true);
+  const [btnDisable, setBtnDisable] = useState(true);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    function buttonAble() {
+      const validEmail = /\S+@\S+\.\S+/;
+      const minOfCaracteres = 6;
+      const minName = 12;
+
+      if (name.length >= minName
+        && password.length >= minOfCaracteres
+        && validEmail.test(email)) {
+        setBtnDisable(false);
+      } else {
+        setBtnDisable(true);
+      }
+    }
+    buttonAble();
+  }, [name, email, password]);
 
   return (
     <div>
       <h1>Register</h1>
       <form>
-        <input type="text" data-testid="common_register__input-name" />
-        <input type="text" data-testid="common_register__input-email" />
-        <input type="password" data-testid="common_register__input-password" />
+        <input
+          type="text"
+          data-testid="common_register__input-name"
+          onChange={ (e) => setName(e.target.value) }
+        />
+        Nome
+        <input
+          type="text"
+          data-testid="common_register__input-email"
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+        Email
+        <input
+          type="password"
+          data-testid="common_register__input-password"
+          onChange={ (e) => setPassword(e.target.value) }
+        />
+        Password
         <button
           type="submit"
-          default={ btnDisable }
+          disabled={ btnDisable }
           data-testid="common_register__button-register"
+          onClick={ (e) => console.log(e) }
         >
-          a
+          desabilitando botao
         </button>
       </form>
     </div>
