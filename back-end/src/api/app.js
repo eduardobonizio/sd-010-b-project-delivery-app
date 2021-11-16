d98678a3c4a89aee9bcaf53650448e5a586bf1a1
Arlen Freitas <arlenfreitas1@gmail.com>
2021-11-16 19:13:41 -0300

    req 11 a 16 semi pronto

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,10 +6,9 @@ const path = require('path');
 const routes = require('./routes/routes');
 // const productsController = require('./controllers/product');
 
-const images = path.join(__dirname, '..', '..', 'public');
 const app = express();
 app.use(bodyParser.json());
-app.use(express.static(images));
+app.use(express.static(path.resolve(__dirname, '..', '..', 'public')));
 app.use(express.json());
 app.use(cors());
 
