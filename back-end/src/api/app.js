496caa4fff9a34428144dd074feb3db3613f67c4
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-16 20:53:23 -0300

    Feat: Add rota producs e emages

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { userRouter, loginRouter } = require('../routes');
+const { userRouter, loginRouter, productsRouter } = require('../routes');
 
 const app = express();
 
@@ -13,6 +13,8 @@ app.use(cors());
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
+app.use('/products', productsRouter);
+app.use('/images', express.static('public'));
 app.use(error);
 
 module.exports = app;
