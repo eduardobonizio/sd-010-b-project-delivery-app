03956dd7e9b377900312f5f83046d247250a3640
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-16 20:56:17 -0300

    Feat: add Router produc

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,7 +1,9 @@
 const userRouter = require('./userRouter');
 const loginRouter = require('./loginRouter');
+const productsRouter = require('./productRouter');
 
 module.exports = {
   userRouter,
   loginRouter,
+  productsRouter,
 };
