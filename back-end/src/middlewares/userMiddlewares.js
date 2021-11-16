6bca5c0ea5750473f71bd04acdfd670ec9e86672
Breno Rodrigues <brenorm@gmail.com>
2021-11-16 18:52:24 -0300

    Requisito 8: Criação de middleware de validação de usuário, que utiliza função helper com regex para validar email

--- back-end/src/middlewares/userMiddlewares.js
+++ back-end/src/middlewares/userMiddlewares.js
@@ -1,10 +1,14 @@
 const rescue = require('express-rescue');
 const md5 = require('md5');
 const User = require('../database/models/user');
+const regexEmail = require('../helpers/regexEmail');
 
 // Comments: Lista de erros
 const errors = {
   passwordWrong: '"password" is wrong',
+  passwordLength: '"password" must have at least 6 characters',
+  nameLength: '"name" field must have at least 8 characters',
+  emailFormat: '"email" must be a valid email',
 };
 
 const validatePassword = rescue(async (req, res, next) => {
@@ -19,4 +23,25 @@ const validatePassword = rescue(async (req, res, next) => {
   next();
 });
 
-module.exports = { validatePassword };
+const validateUser = rescue(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  if (name.length < 8) {
+    return res.status(400).json({ message: errors.nameLength });
+  }
+  
+  if (!regexEmail(email)) {
+    return res.status(400).json({ message: errors.emailFormat });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({ message: errors.passwordLength });
+  }
+
+  next();
+});
+
+module.exports = {
+  validatePassword,
+  validateUser,
+};
