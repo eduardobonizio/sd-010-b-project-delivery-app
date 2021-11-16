180c298f4cafe5a82ac130a29018c374693944aa
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-16 19:52:51 -0300

    refactor(App): rota da página checkout
    - adicionando a rota para a página de Checkout

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Checkout from './pages/Checkout';
 import './styles/App.css';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={ <Login /> } />
+      <Route exact path="/customer/checkout" element={ <Checkout /> } />
     </Routes>
   );
 }
