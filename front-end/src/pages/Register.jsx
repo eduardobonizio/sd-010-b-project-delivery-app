ee1adb75b252ccf88b2bbfb39adbaf4543941975
CIGR2021 <appencigr@gmail.com>
2021-11-16 17:25:43 -0300

    Inclusão dos data-testids

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import ErrorRegister from '../components/ErrorRegister';
 
 function Register() {
   return (
     <form>
-      <input type="text" name="nome" id="nome" />
-      <input type="email" name="e-mail" id="e-mail" />
-      <input type="password" name="password" id="password" />
-      <input type="button" value="Cadastrar" />
+      <input
+        data-testid="common_register__input-name"
+        type="text"
+        name="nome"
+        id="nome"
+      />
+      <input
+        data-testid="common_register__input-email"
+        type="email"
+        name="e-mail"
+        id="e-mail"
+      />
+      <input
+        data-testid="common_register__input-password"
+        type="password"
+        name="password"
+        id="password"
+      />
+      <input
+        data-testid="common_register__button-register"
+        type="button"
+        value="Cadastrar"
+      />
+      { errorMsg ? <ErrorRegister /> : '' }
     </form>
   );
 }
