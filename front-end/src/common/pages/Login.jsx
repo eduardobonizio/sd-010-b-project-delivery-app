ab5e38a92c1716597e6b71826253ee1d2ece5d0f
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-11-16 16:25:34 -0300

    Feat: test user inputs on login screen

--- front-end/src/common/pages/Login.jsx
+++ front-end/src/common/pages/Login.jsx
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../../styles/login.css';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabledBtn, setDisabledBtn] = useState(true);
+
+  const validationEmailPwd = () => {
+    const regex = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const Email = regex.test(email);
+    console.log(Email);
+    const Pwd = password.length;
+    const minPwdLength = 6;
+
+    if (Email && Pwd >= minPwdLength) {
+      setDisabledBtn(false);
+    } else {
+      setDisabledBtn(true);
+    }
+  };
+
+  useEffect(validationEmailPwd, [email, password]);
+
   return (
     <div className="login-content">
       <form action="">
         <div>
           <label htmlFor="email">
             Login
-            <input type="text" name="email" data-testid="common_login__input-email" />
+            <input
+              autoComplete="off"
+              type="text"
+              name="email"
+              data-testid="common_login__input-email"
+              onChange={ (e) => setEmail(e.target.value) }
+            />
           </label>
           <label htmlFor="password">
             Senha
             <input
+              autoComplete="off"
               type="password"
               name="password"
               data-testid="common_login__input-password"
+              onChange={ (e) => setPassword(e.target.value) }
             />
           </label>
           <button
             type="submit"
             data-testid="common_login__button-login"
+            disabled={ disabledBtn }
           >
             LOGIN
           </button>
