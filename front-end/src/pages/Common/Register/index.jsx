8e2327e9cc5ee1097979eb5959a28f76b52b942a
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-16 13:25:54 -0300

    fix - finalizando requisito 7

--- front-end/src/pages/Common/Register/index.jsx
+++ front-end/src/pages/Common/Register/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Register() {
+  const [btnDisable] = useState(true);
+
   return (
     <div>
       <h1>Register</h1>
+      <form>
+        <input type="text" data-testid="common_register__input-name" />
+        <input type="text" data-testid="common_register__input-email" />
+        <input type="password" data-testid="common_register__input-password" />
+        <button
+          type="submit"
+          default={ btnDisable }
+          data-testid="common_register__button-register"
+        >
+          a
+        </button>
+      </form>
     </div>
   );
 }
