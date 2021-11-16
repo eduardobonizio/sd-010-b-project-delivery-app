12e8044a33fa010663bc264c0afa1064bf82e244
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-11-16 15:19:47 -0300

    Atualizar - Header - Refatoração nas tags, utilização de span ao invéz de li, criação da opção de sair da conta

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,34 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export default function Header(props) {
   const { pageName, yourOrder, userName } = props;
 
+  function logoutUser() {
+    localStorage.clear();
+  }
+
   function structure(status) {
     if (status === 'MEUS PEDIDOS') {
       return (
-        <ul>
-          <li
+        <>
+          <span
             data-testid="customer_products__element-navbar-link-products"
           >
             {pageName}
-          </li>
-          <li
+          </span>
+          <span
             data-testid="customer_products__element-navbar-link-orders"
           >
             MEUS PEDIDOS
-          </li>
-          <li
+          </span>
+          <span
             data-testid="customer_products__element-navbar-user-full-name"
           >
             {userName}
-          </li>
-          <li
+          </span>
+          <span
+            type="button"
             data-testid="customer_products__element-navbar-link-logout"
+            onClick={ logoutUser }
           >
-            Sair
-          </li>
-        </ul>
+            <Link to='/login'>
+              Sair
+            </Link>
+          </span>
+        </>
       );
     }
   }
