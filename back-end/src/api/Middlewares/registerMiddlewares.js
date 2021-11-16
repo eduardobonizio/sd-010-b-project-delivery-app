26e32047bd30fb828a340c8c03002d2c64a86fc0
Thiago Leite <leite.tjs@gmail.com>
2021-11-16 16:32:22 -0300

    register middleware done

---
+++ back-end/src/api/Middlewares/registerMiddlewares.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+const validarLogin = (req, res, next) => {
+  const { email, password, name } = req.body;
+  const { error } = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    name: Joi.string().min(12).required(),
+  }).validate({ email, password, name });
+
+  if (error) {
+    return res.status(400).json(error.details[0].message);
+  }
+
+  next();
+};
+
+module.exports = {
+  validarLogin,
+};
