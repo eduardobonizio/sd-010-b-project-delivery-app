ed4ce8e21032e3d946365be14a5a7c98d4666e12
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-11-16 20:39:53 -0300

    FIX: corrigindo conflitos do merge

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
-<<<<<<< HEAD
 import Login from './pages/login';
 import CustumerProducts from './pages/custumerProducts';
-
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/"><Redirect to="/login" /></Route>
-      <Route exact path="/login" component={ Login } />
-      <Route exact path="/customer/products" component={ CustumerProducts } />
-    </Switch>
-=======
-import Login from './pages/Login';
 import Register from './pages/Register';
 import UsersProvider from './context/Users/UsersProvider';
 import ProductsProvider from './context/Products/ProductsProvider';
@@ -23,14 +12,13 @@ function App() {
     <ProductsProvider>
       <UsersProvider>
         <Switch>
-          <Route path="/customer/products" component={ Login } />
+        <Route exact path="/customer/products" component={ CustumerProducts } />
           <Route path="/login" component={ Login } />
           <Route path="/register" component={ Register } />
           <Route exact path="/"><Redirect to="/login" /></Route>
         </Switch>
       </UsersProvider>
     </ProductsProvider>
->>>>>>> 7721979d9350c3f8aeb1082a0650371dfc19a033
   );
 }
 
