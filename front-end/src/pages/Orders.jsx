84594b2820b63a29f6fec9cdfca6caa5cb644bb9
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-17 18:25:08 -0300

    feat: faz a pagina de pedidos do usuario e nela faz o fetch pra api

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import APITOKEN from '../api/index';
+import OrderCard from '../components/OrderCard';
 
 function Orders() {
+  const [ordersState, setOrdersState] = useState([]);
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem('user'));
+    const { token } = userInfo;
+    APITOKEN.fetchOrders(token).then((data) => setOrdersState(data));
+  }, [ordersState]);
+
+  console.log(ordersState);
   return (
     <div>
       <h1>Meus Pedidos</h1>
+      {
+        ordersState.map((order) => <OrderCard key={ order.id } order={ order } />)
+      }
     </div>
   );
 }
