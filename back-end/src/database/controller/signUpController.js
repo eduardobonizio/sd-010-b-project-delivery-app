3925a05a48ac92f7557957be929cb5b6ac187cf4
Cristian Bugs <cristian.bugs@gmail.com>
2021-11-17 16:59:51 -0300

    axios to tasks 9 and 10

--- back-end/src/database/controller/signUpController.js
+++ back-end/src/database/controller/signUpController.js
@@ -4,14 +4,20 @@ const md5 = require('md5');
 const { createUser, checkUserExists } = require('../service')
 
 router.post('/', async (req, res) => {
-const { name, email, password } = req.body;
-const check =  await checkUserExists(name, email);
-if (check) {
-  return res.status(409).json({ message: 'User already exists'})
+  const { name, email, password } = req.body;
+  try {
+  const check =  await checkUserExists(name, email);
+  if (check) {
+    return res.status(409).json({ message: 'User already exists'})
+  }
+  const crypto = md5(password);
+  const role = 'customer';
+  await createUser(name, email, crypto, role);
+    return res.status(201).json({ message: 'Created' })
 }
-const crypto = md5(password);
-await createUser(name, email, crypto);
-  return res.status(201).json({ message: 'Created'})
+  catch (err) {
+  console.log(err);
+  }
 });
 
 module.exports = router;
---
+++ back-end/src/database/service/jwt.js
@@ -0,0 +1,15 @@
+// const jwt = require('jsonwebtoken');
+// const jwtKey = require("fs")
+//   .readFileSync("../back-end/jwt.evaluation.key", { encoding: "utf-8" })
+//   .trim();
+
+// const jwtConfig = {
+//     expiresIn: '1d',
+//     algorithm: 'HS256',
+// };
+
+// const jwtLogin =  (check) => {
+//     const token = jwt.sign({ data: check }, jwtKey, jwtConfig);
+//     return token;
+//     };
+// module.exports = { jwtLogin };
