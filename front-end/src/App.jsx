14816ed6d39ae60e61bfa3a1a0c171f5205092cf
Thiago Leite <leite.tjs@gmail.com>
2021-11-17 14:58:08 -0300

    accepting incoming changes from main branch

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,22 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
 import RegisterForm from './components/RegisterForm';
-<<<<<<< HEAD
-import Products from './pages/Products';
-=======
 import Home from './pages/Home';
->>>>>>> b8b779aac09c0d2ee7e246fa663ffbd3c6ba3c2c
 
 function App() {
   return (
     <Routes>
       <Route exact path="/login" element={ <LoginPage /> } />
       <Route exact path="/register" element={ <RegisterForm /> } />
-<<<<<<< HEAD
-      <Route exact path="products" element={ <Products /> } />
-=======
       <Route exact path="/" navigate element={ <Home /> } />
->>>>>>> b8b779aac09c0d2ee7e246fa663ffbd3c6ba3c2c
     </Routes>
   );
 }
