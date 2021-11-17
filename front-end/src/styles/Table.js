1e1c15a282c09f325bdd7f1e7aaa2466e3aef986
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-17 16:33:47 -0300

    estilização do header

--- front-end/src/styles/Table.js
+++ front-end/src/styles/Table.js
@@ -1,13 +1,20 @@
 import styled from 'styled-components';
 
 export const table = styled.table`
-  background-color: red;
+  width: 90%;
+  margin: 10px auto;
+  border: 1px solid #ccc;
 `;
 
 export const tr = styled.tr`
-  background-color: blue;
+  background-color: white;
 `;
 
 export const th = styled.th`
-  color: white;
+  font-size: 1em;
+`;
+
+export const thead = styled.thead`
+  width: 100%;
+  text-align: center;
 `;
