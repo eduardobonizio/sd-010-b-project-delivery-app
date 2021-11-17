1f4eea35d567127f04f97f400aa4e99ed800564c
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-16 22:13:40 -0300

    refactor(customer checkout): propTypes no componente TableHeader

--- front-end/src/components/Checkout/TableHeader.jsx
+++ front-end/src/components/Checkout/TableHeader.jsx
@@ -1,12 +1,16 @@
-/* eslint-disable react/prop-types */
+import PropTypes from 'prop-types';
 import React from 'react';
 
-function TableHeader({ headerItems = [] }) {
+function TableHeader({ headerItems }) {
   return (
     <div className="table-header">
       { headerItems.map((value) => <div key={ value }>{value}</div>)}
     </div>
   );
 }
+
+TableHeader.propTypes = {
+  headerItems: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default TableHeader;
