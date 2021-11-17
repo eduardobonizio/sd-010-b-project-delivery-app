788fae2ec4bde3cb935256643e263c64681cb7c8
Jessie <miss_sc_jessi@hotmail.com>
2021-11-17 16:51:59 -0300

    lint

--- front-end/src/services/product.js
+++ front-end/src/services/product.js
@@ -2,4 +2,6 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:3001/';
 
-export const getProducts = () => axios.get(`${API_URL}cliente/produto`);
+const getProducts = () => axios.get(`${API_URL}cliente/produto`);
+
+export default getProducts;
