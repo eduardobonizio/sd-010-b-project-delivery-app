5fb6a80169c4fd48028232dc2504b876c518f1b4
legalvao1 <legalvao1@gmail.com>
2021-11-17 09:36:00 -0300

    Adicionando o valor total na pagina de checkout

--- front-end/src/components/checkoutTable.jsx
+++ front-end/src/components/checkoutTable.jsx
@@ -4,11 +4,17 @@ import Tbody from './Tbody';
 import Thead from './Thead';
 
 function Table() {
+  const getTotal = () => {
+    const carrinho = JSON.parse(localStorage.getItem('carrinho'));
+    const total = Object.values(carrinho).reduce((acc, curr) => acc + curr);
+    return total.toFixed(2).replace(/\./, ',');
+  };
+
   return (
     <table>
       <Thead />
       <Tbody />
-      <p data-testid="customer_checkout__element-order-total-price">total</p>
+      <p data-testid="customer_checkout__element-order-total-price">{ getTotal() }</p>
       <Address />
     </table>
   );
