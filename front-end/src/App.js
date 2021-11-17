edb40fcac00c804263458d7f6210456e41ac39b0
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-17 16:12:41 -0300

    feat: cria um componente de card de pedidos do cliente

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Orders from './pages/Orders';
 import Products from './pages/Products';
 import Register from './pages/Register';
 
@@ -18,6 +19,7 @@ function App() {
         </Route>
         <Route exact path="/login" component={ Login } />
         <Route exact path="/customer/products" component={ Products } />
+        <Route exact path="/customer/orders" component={ Orders } />
         <Route exact path="/register" component={ Register } />
       </Switch>
     </div>
