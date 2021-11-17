d0bcaf0d6337baf322dcac39dff99c307c4175b4
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-17 16:34:37 -0300

    criação do style component da pagina detalhes de produto

---
+++ front-end/src/styles/PageOrderDetails.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const divPageOrderDetails = styled.div`
+  background-color: yellow;
+`;
+
+export const a = styled.table`
+  background-color: red;
+`;
