ddd056561efeda533265373b23c997b9331c9a8e
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-11-17 16:11:45 -0300

    Feat: login not found backend

--- back-end/src/routes/loginRouter.js
+++ back-end/src/routes/loginRouter.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
 
+const LoginService = require('../service/login');
+
 router.get('/', (req, res) => res.status(200).send({ message: 'everything ok!!!' }));
+router.post('/', async (req, res) => {
+  const { login, password } = req.body;
+
+  const user = await LoginService.logged(login, password);
+
+  if (!user) return res.status(404).send();
+});
 
 module.exports = router;
