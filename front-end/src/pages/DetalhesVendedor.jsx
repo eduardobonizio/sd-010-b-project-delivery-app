30f08daf9f835f8e4499097a022ee24899d2e6b1
esionascimento <esio.392@protonmail.com>
2021-11-17 17:52:54 -0400

    new page para vendedor detalhes

---
+++ front-end/src/pages/DetalhesVendedor.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function DetalhesVendedor() {
+  return (
+    <div>
+      <nav>
+        <p>PEDIDOS</p>
+        <p>Sair</p>
+      </nav>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default DetalhesVendedor;
