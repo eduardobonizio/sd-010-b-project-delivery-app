8e847a820ff6780454bda91abe8cca7f665c5ed5
Lucas Ribeiro <lucasrb04@gmail.com>
2021-11-17 17:32:17 -0300

    add: Backend - adicionando servidor do socket

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -5,10 +5,21 @@ const error = require('../middleware/error');
 const routes = require('./routes');
 
 const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3001', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  }});
+
 app.use(express.json());
 
 app.use(cors());
 app.use(express.static('public'));
+
+require('../sockets/status')(io);
 
 app.use('/login', routes.login);
 app.use('/register', routes.register);
