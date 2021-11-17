62ec9f8ff2ab392d48862d9a760a9a339195f565
legalvao1 <legalvao1@gmail.com>
2021-11-17 12:01:42 -0300

    Removendo itens do carrinho da pagina de checkout

--- front-end/src/components/Tbody.jsx
+++ front-end/src/components/Tbody.jsx
@@ -1,35 +1,50 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import getProducts from '../services/products';
 
-function Tbody() {
+function Tbody(props) {
   const [products, setProducts] = useState([]);
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const cart = JSON.parse(localStorage.getItem('carrinho'));
   const cartItems = Object.entries(cart).filter((item) => item[1] > 0);
 
   const renderProducts = (resultData) => {
     const productData = [];
-
     cartItems.forEach((i) => resultData.forEach((p) => {
       if (p.id === parseInt(i[0], 10)) productData.push(p);
     }));
-
-    return productData;
+    setItems(productData);
   };
 
   const fetchData = async () => {
     const result = await getProducts();
     const resultData = await result.data;
     setProducts(resultData);
+    setLoading(false);
+  };
+
+  const removeItem = (index, id) => {
+    delete cart[id];
+    cartItems.splice(index, 1);
+    localStorage.setItem('carrinho', JSON.stringify(cart));
+    renderProducts(products);
+    props.getTotal();
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+    renderProducts(products);
+  }, [loading]);
+
+  if (loading) return <p>Carregando</p>;
+  if (items.length === 0) return <p>vazio</p>;
 
   return (
     <tbody>
       {
-        renderProducts(products).map((prod, index) => (
+        items.map((prod, index) => (
           <tr key={ index }>
             <td
               data-testid={
@@ -62,6 +77,7 @@ function Tbody() {
               <button
                 data-testid={ `customer_checkout__element-order-table-remove-${index}` }
                 type="button"
+                onClick={ () => removeItem(index, prod.id) }
               >
                 Remover
               </button>
@@ -72,5 +88,9 @@ function Tbody() {
     </tbody>
   );
 }
+
+Tbody.propTypes = {
+  getTotal: PropTypes.func.isRequired,
+};
 
 export default Tbody;
--- front-end/src/components/checkoutTable.jsx
+++ front-end/src/components/checkoutTable.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Address from './address';
 import Tbody from './Tbody';
 import Thead from './Thead';
 
 function Table() {
+  const [total, setTotal] = useState('0,00');
+
   const getTotal = () => {
     const carrinho = JSON.parse(localStorage.getItem('carrinho'));
-    const total = Object.values(carrinho).reduce((acc, curr) => acc + curr);
-    return total.toFixed(2).replace(/\./, ',');
+    if (Object.entries(carrinho).length !== 0) {
+      const calculate = Object.values(carrinho).reduce((acc, curr) => acc + curr);
+      return setTotal(calculate.toFixed(2).replace(/\./, ','));
+    }
+    return setTotal('0,00');
   };
+
+  useEffect(() => {
+    getTotal();
+  }, []);
 
   return (
     <table>
       <Thead />
-      <Tbody />
-      <p data-testid="customer_checkout__element-order-total-price">{ getTotal() }</p>
+      <Tbody getTotal={ getTotal } />
+      <p data-testid="customer_checkout__element-order-total-price">{ total }</p>
       <Address />
     </table>
   );
