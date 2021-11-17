26a55caad8590ec17e37f96601809024eeaeac98
André De Bem <78712956+andredebem@users.noreply.github.com>
2021-11-17 14:53:48 -0300

    Update back-end/src/api/services/loginService.js

--- back-end/src/api/services/loginService.js
+++ back-end/src/api/services/loginService.js
@@ -1,7 +1,5 @@
 const md5 = require('md5');
 const { User } = require('../../database/models');
-
-// const {ApiError} = require('../ut')
 const { ApiError } = require('../utils/ApiError');
 
 const login = async (body) => {
