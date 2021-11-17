e368107e30749b062ce6070570a0fd87bf22dd88
CIGR2021 <appencigr@gmail.com>
2021-11-17 10:20:58 -0300

    uso do contexto no Register.

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../context/Context';
 import ErrorRegister from '../components/ErrorRegister';
 
 function Register() {
