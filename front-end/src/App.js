81037c4bb56af595239f6d074f647ab03191b5db
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-17 19:47:24 -0300

    feat: implementa a logica para na pagina de order details ele receber o id do pedido pelos parametros da url

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import OrderDetails from './pages/OrderDetails';
 import Orders from './pages/Orders';
 import Products from './pages/Products';
 import Register from './pages/Register';
@@ -20,6 +21,7 @@ function App() {
         <Route exact path="/login" component={ Login } />
         <Route exact path="/customer/products" component={ Products } />
         <Route exact path="/customer/orders" component={ Orders } />
+        <Route exact path="/customer/orders/:id" component={ OrderDetails } />
         <Route exact path="/register" component={ Register } />
       </Switch>
     </div>
