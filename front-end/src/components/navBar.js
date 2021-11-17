778c1aefde7547c9a757c428c236a13d368a2c93
isTorres <ismaeltorres612@gmail.com>
2021-11-17 18:02:57 -0300

    style: component navbar

--- front-end/src/components/navBar.js
+++ front-end/src/components/navBar.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-max-depth */
+/* eslint-disable max-len */
 import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
@@ -15,22 +17,30 @@ export default function NavBar({ isCustomer, nameButtonOrder, linkOrder }) {
   }, []);
 
   return (
-    <nav>
+    <nav className="flex justify-between items-center m-5 text-xl font-semibold">
+      <div className="relative flex items-center justify-between mx-10 h-16">
+        <img className="cursor-pointer w-52" src="/logo.svg" alt="Imagem da logo" />
+      </div>
       { isCustomer
         && (
           <Link to="/customer/products">
-            <div data-testid="customer_products__element-navbar-link-products">
+            <div className="bg-yellow-color mx-10 px-10 py-1 rounded-md" data-testid="customer_products__element-navbar-link-products">
               Produtos
             </div>
           </Link>)}
 
       <Link to={ linkOrder }>
-        <div data-testid="customer_products__element-navbar-link-orders">
+        <div className="bg-yellow-color ml-10 px-10 py-1 rounded-md" data-testid="customer_products__element-navbar-link-orders">
           {nameButtonOrder}
         </div>
       </Link>
-
-      <div data-testid="customer_products__element-navbar-user-full-name">
+      <div
+        data-testid="customer_products__element-navbar-user-full-name"
+        className="flex items-center mr-5 ml-auto"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 mx-2 text-yellow-color" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+        </svg>
         {userData.name}
       </div>
 
@@ -39,6 +49,7 @@ export default function NavBar({ isCustomer, nameButtonOrder, linkOrder }) {
           type="button"
           onClick={ () => { localStorage.removeItem('user'); } }
           data-testid="customer_products__element-navbar-link-logout"
+          className="rounded-md bg-dark-color text-yellow-color mx-10 px-5 py-1"
         >
           Sair
         </button>
