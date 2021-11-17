0ede3af06fe6d308c0a711d8a723c4f74eddfd68
Jessie <miss_sc_jessi@hotmail.com>
2021-11-17 20:08:50 -0300

    login enviando o token e outras infos

--- back-end/src/controllers/users.js
+++ back-end/src/controllers/users.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const rescue = require('express-rescue');
-const { getUserByEmail } = require('../services/users');
+const { loginUser } = require('../services/users')
 
 const userRouter = express.Router();
 
 userRouter.post('/', rescue(async (req, res) => {
-  const { email } = req.body;
-  const user = await getUserByEmail(email);
-
-  if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
-  return res.status(200).json(user);
+  const login = await loginUser(req.body);
+  if (login.err) return res.status(404).json(login.err);
+  return res.status(200).json(login);
 }));
 
 module.exports = { userRouter };
