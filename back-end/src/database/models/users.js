9944ef73983194a4bd03fbed2b4bff9f6954e754
Joao Vanelli <vanellijoao@gmail.com>
2021-11-17 15:44:31 -0300

    fixing Sequelize

--- back-end/src/database/models/users.js
+++ back-end/src/database/models/users.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-  const users = sequelize.define('user', {
+  const user = sequelize.define('user', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
@@ -9,11 +9,11 @@ module.exports = (sequelize, DataTypes) => {
     tableName: 'users',
   });
 
-  users.associate = (models) => {
-    users.hasMany(models.sales,
-      { foreignKey: 'user_id', as: 'users' },
-      { foreignKey: 'seller_id', as: 'sellers' }
+  user.associate = (models) => {
+    user.hasMany(models.sale,
+      { foreignKey: 'user_id', as: 'user' },
+      { foreignKey: 'seller_id', as: 'seller' }
     );
   };
-  return users;
+  return user;
 };
