6ac76a501220c4d5b620acd7027b427d3fd4b240
Lucas Ribeiro <lucasrb04@gmail.com>
2021-11-18 00:48:59 -0300

    add: Backend - Adicionando rota de pegar todos os usuarios, apagar usuarios

--- back-end/src/controllers/Users.js
+++ back-end/src/controllers/Users.js
@@ -3,4 +3,10 @@ const { usersService } = require('../services');
 const getAllSellers = async (_req, res) => usersService.getAllSellers()
   .then(({ status, data }) => res.status(status).json({ data }));
 
-module.exports = { getAllSellers };
+const getAllUsers = async (_req, res) => usersService.getAllSellers()
+  .then(({ status, data }) => res.status(status).json({ data }));
+
+const deleteUser = async (req, res) => usersService.deleteUser(req.params)
+  .then(({ status, data }) => res.status(status).json({ data }));
+
+module.exports = { getAllSellers, getAllUsers, deleteUser };
--- back-end/src/services/Users.js
+++ back-end/src/services/Users.js
@@ -3,8 +3,21 @@ const httpStatus = require('http-status');
 const { User } = require('../database/models');
 
 const getAllSellers = async () => {
-  const data = await User.findAll({ where: { role: 'seller' }, attributes: { exclude: ['password'] } });
+  const data = await User.findAll({ 
+    where: { role: 'seller' },
+    attributes: { exclude: ['password'] }, 
+  });
   return ({ status: httpStatus.OK, data });
 };
 
-module.exports = { getAllSellers };
+const getAllUsers = async () => {
+  const data = await User.findAll({ attributes: { exclude: ['password'] } });
+  return ({ status: httpStatus.OK, data });
+};
+
+const deleteUser = async ({ id }) => {
+  const data = await User.destroy({ where: { id } });
+  return ({ status: httpStatus.OK, data });
+};
+
+module.exports = { getAllSellers, getAllUsers, deleteUser };
