0d067e6c4896dafc5253fb02af222ab6f581b3b1
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-11-18 19:49:28 -0300

    feat: create checkout page

---
+++ front-end/src/pages/Checkout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Checkout() {
+  return (
+    <main>
+      <header>
+        <h1>Tela de Checkout</h1>
+      </header>
+    </main>
+  );
+}
+
+export default Checkout;
