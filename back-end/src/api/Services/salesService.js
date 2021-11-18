28b6a6d874b72222405e49f1490077b9dbaefce4
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-11-18 13:37:08 -0300

    endPoint getAll sales do usuario sem msg de erro

--- back-end/src/api/Services/salesService.js
+++ back-end/src/api/Services/salesService.js
@@ -4,7 +4,7 @@ const getAllSales = async (id) => {
   const query = { where: { id } };
 
   const allSales = await sale.findAll(query);
-  if (allSales.length === 0) return { message: 'No momento você não possui compras' };
+  // if (allSales.length === 0) return { message: 'No momento você não possui compras' };
 
   return allSales;
 };
