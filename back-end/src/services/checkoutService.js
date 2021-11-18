3f4a8b88427dc55e23759954524af8e3ce3e01c3
Huggo Parcelly <h.parcelly@gmail.com>
2021-11-17 23:20:45 -0300

    adiciona requisicao ao endpoint de cadastro do pedido

--- back-end/src/services/checkoutService.js
+++ back-end/src/services/checkoutService.js
@@ -1,10 +1,8 @@
 const { Sale, User } = require('../database/models');
 
 const createSale = async (body, user_id) => {
-  // DÚVIDA userId e sellerID.
-
-  // recebe o token total_price address number status( 'pendente', só quem pode mudar é o vendedor ou admin)
-  const { seller_id= '1', total_price, delivery_address, delivery_number, status } = body;
+  console.log(body);
+  const { seller_id, total_price, delivery_address, delivery_number, status } = body;
   const dateNow = new Date();
   
   // criar a venda
