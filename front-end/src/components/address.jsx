e62f00a20c1307fff475a205a8bb1ccc028aeefd
Daniel <dsouzaborini@gmail.com>
2021-11-18 09:26:02 -0300

    configura req post

--- front-end/src/components/address.jsx
+++ front-end/src/components/address.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { getUsers } from '../services/user';
 
-function Address() {
+function Address(props) {
   const [users, setUsers] = useState([]);
+
+  const { clickFuntion, postFunction } = props;
 
   const fetchData = async () => {
     const usersData = await getUsers();
@@ -18,31 +21,58 @@ function Address() {
     <main>
       <form>
         <div>
-          <select data-testid="customer_checkout__select-seller">
-            {users.map((user, index) => <option key={ index }>{ user.name }</option>)}
+          <select
+            data-testid="customer_checkout__select-seller"
+            onChange={ clickFuntion }
+            name="userName"
+          >
+            <option value="null">Selecione um vendedor</option>
+            {users.map((user, index) => (
+              <option
+                key={ index }
+                value={ user.id }
+              >
+                { user.name }
+              </option>))}
           </select>
         </div>
         <div>
           <label htmlFor="endereço">
             Endereço
-            <input data-testid=" customer_checkout__input-address" type="text" />
+            <input
+              data-testid=" customer_checkout__input-address"
+              type="text"
+              name="address"
+              onChange={ clickFuntion }
+            />
           </label>
         </div>
         <div>
           <label htmlFor="numero">
             Número
-            <input data-testid="customer_checkout__input-addressNumber" type="text" />
+            <input
+              data-testid="customer_checkout__input-addressNumber"
+              type="text"
+              name="number"
+              onChange={ clickFuntion }
+            />
           </label>
         </div>
       </form>
       <button
         data-testid="customer_checkout__button-submit-order"
         type="submit"
+        onClick={ postFunction }
       >
         Finalizar pedido
       </button>
     </main>
   );
 }
+
+Address.propTypes = {
+  clickFuntion: PropTypes.func.isRequired,
+  postFunction: PropTypes.func.isRequired,
+};
 
 export default Address;
--- front-end/src/components/checkoutTable.jsx
+++ front-end/src/components/checkoutTable.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router';
 import Address from './address';
 import Tbody from './Tbody';
 import Thead from './Thead';
+import { postSales } from '../services/sales';
 
 function Table() {
   const [total, setTotal] = useState('0,00');
+  const [adress, setAdress] = useState('');
+  const [adressNumber, setAdressNumber] = useState('');
+  const [sellerId, setSellerId] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  const [saleId, setSaleId] = useState('');
 
   const getTotal = () => {
     const carrinho = JSON.parse(localStorage.getItem('carrinho'));
@@ -15,16 +22,46 @@ function Table() {
     return setTotal('0,00');
   };
 
+  const onClick = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'address') setAdress(value);
+    if (name === 'number') setAdressNumber(value);
+    if (name === 'userName') setSellerId(value);
+  };
+
+  const postSale = async (e) => {
+    e.preventDefault();
+    const { id } = JSON.parse(localStorage.getItem('user'));
+    const newSaleBody = {
+      userId: id,
+      sellerId,
+      totalPrice: total,
+      deliveryAddress: adress,
+      deliveryNumber: adressNumber,
+      status: 'Pendente',
+    };
+
+    const resp = await postSales(newSaleBody);
+    setSaleId(resp);
+    console.log('chamei');
+    return setRedirect(true);
+  };
+
   useEffect(() => {
     getTotal();
   }, []);
 
+  if (redirect) return <Redirect to={ `/customer/orders/${saleId}` } />;
   return (
     <table>
+      <p>{adress}</p>
+      <p>{adressNumber}</p>
+      <p>{sellerId}</p>
       <Thead />
       <Tbody getTotal={ getTotal } />
       <p data-testid="customer_checkout__element-order-total-price">{ total }</p>
-      <Address />
+      <Address clickFuntion={ onClick } postFunction={ postSale } />
     </table>
   );
 }
