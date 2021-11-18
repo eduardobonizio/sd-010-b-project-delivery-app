2ffdfdb07e4afde888ca22c115152921e2bbc2ee
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-18 00:35:13 -0300

    f

---
+++ front-end/src/components/TableDetails/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function TableDetails() {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Item</th>
+          <th>Descrição</th>
+          <th>Quantidade</th>
+          <th>Valor Unitário</th>
+          <th>Sub-total</th>
+        </tr>
+      </table>
+    </div>
+  );
+}
