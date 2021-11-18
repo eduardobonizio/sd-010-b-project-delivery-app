245b119bcdfc036c0774a419122ab9291b090f01
Thiago Leite <leite.tjs@gmail.com>
2021-11-18 17:12:39 -0300

    orderdetails component

--- front-end/src/components/OrderDetails.jsx
+++ front-end/src/components/OrderDetails.jsx
@@ -1,31 +1,65 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Loading from './Loading';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+// import axios from 'axios';
+// import Loading from './Loading';
 
-function OrderDetails() {
-  const [productDetails, setProductDetails] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const id = 3;
+function OrderDetails({ id }) {
+  const [productDetails] = useState([
+    { d: 3, description: 'legal', quantity: 3, unityValue: 7.90, total: 7.90 },
+    { d: 4, description: 'legal', quantity: 3, unityValue: 7.90, total: 7.90 }]);
+  console.log(productDetails);
+  // const [loading, setLoading] = useState(true);
+  console.log(id);
+  const number3 = 3;
+  const vendedor = 'Thiago Leite';
+  const data = 'hoje';
+  const status = 'entregue';
 
-  useEffect(() => {
-    const fetchProductDetails = async () => {
-      const productInfo = await axios.get(`http://localhost:3001/${id}`);
-      console.log(productInfo);
-      setProductDetails(productInfo);
-      setLoading(false);
-    };
-    fetchProductDetails();
-  });
+  // useEffect(() => {
+  //   const fetchProductDetails = async () => {
+  //     const productInfo = await axios.get('http://google.com.br');
+  //     console.log(productInfo);
+  //     setProductDetails([{ id: 1, description: 'legal' }]);
+  //     setLoading(false);
+  //   };
+  //   fetchProductDetails();
+  // });
 
-  if (loading) {
-    return <Loading />;
-  }
+  // if (loading) {
+  //   return <Loading />;
+  // }
 
   return (
-    <div>
-      {productDetails}
-    </div>
+    <main>
+      Detalhes do pedido
+      <div>
+        {`PEDIDO ${number3}; Vendedor(a): ${vendedor}; Data ${data} ${status}`}
+        <button type="button">MARCAR COMO ENTREGUE</button>
+      </div>
+      <div>
+        {
+          productDetails.map((product) => (
+            // eslint-disable-next-line max-len
+            <span key={ product.d }>
+              <span>{product.d}</span>
+              <span>{product.description}</span>
+              <span>{product.quantity}</span>
+              <span>{product.unityValue}</span>
+              <span>{product.total}</span>
+            </span>
+          ))
+        }
+      </div>
+    </main>
   );
 }
+
+OrderDetails.propTypes = {
+  id: PropTypes.string,
+};
+
+OrderDetails.defaultProps = {
+  id: '',
+};
 
 export default OrderDetails;
