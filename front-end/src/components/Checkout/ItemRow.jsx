cbf314fb5e534a2f48420536aff11b99e678b723
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-17 22:18:36 -0300

    refactor(ItemRow): renderizando o item do carrinho
    - recebe o objeto contendo o índice do array
    - recebe o objeto contendo um objeto contendo as informações do produto no carrinho
    - renderizando as propriedades de cada item do carrinho dinamicanamente
    - adicionando propTypes

--- front-end/src/components/Checkout/ItemRow.jsx
+++ front-end/src/components/Checkout/ItemRow.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import dataTestIdDict from '../../utils/dataTestIdDict';
 
 const {
@@ -10,17 +11,33 @@ const {
   dataTestId27,
 } = dataTestIdDict;
 
-function ItemRow() {
+function ItemRow({ cartItem }) {
+  const { item, index } = cartItem;
+  const { id, name, preco, quantity, subTotal } = item;
+
   return (
     <div className="item-row">
-      <div data-testid={ dataTestId22 }>1</div>
-      <div data-testid={ dataTestId23 }>Salgadinho Torcida Churrasco</div>
-      <div data-testid={ dataTestId24 }>1</div>
-      <div data-testid={ dataTestId25 }>R$ 3,50</div>
-      <div data-testid={ dataTestId26 }>R$ 10,50</div>
-      <div data-testid={ dataTestId27 }>Remover</div>
+      <div data-testid={ `${dataTestId22}${index}` }>{id}</div>
+      <div data-testid={ `${dataTestId23}${index}` }>{name}</div>
+      <div data-testid={ `${dataTestId24}${index}` }>{quantity}</div>
+      <div data-testid={ `${dataTestId25}${index}` }>{`R$ ${preco}`}</div>
+      <div data-testid={ `${dataTestId26}${index}` }>{subTotal}</div>
+      <div data-testid={ `${dataTestId27}${index}` }>Remover</div>
     </div>
   );
 }
+
+ItemRow.propTypes = {
+  cartItem: PropTypes.shape({
+    item: PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      preco: PropTypes.string,
+      quantity: PropTypes.number,
+      subTotal: PropTypes.number,
+    }),
+    index: PropTypes.number,
+  }).isRequired,
+};
 
 export default ItemRow;
