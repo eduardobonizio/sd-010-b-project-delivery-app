7d130229399f92126189a5aba64a01b802e04eae
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-17 22:32:45 -0300

    refactor(OrdersItemsTable): renderizando carrinho no localStorage
    - adicionando estado local armazenando o carrinho do localStorage
    - renderizando os items do carrinho

--- front-end/src/components/Checkout/OrderItemsTable.jsx
+++ front-end/src/components/Checkout/OrderItemsTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TableHeader from './TableHeader';
 import ItemRow from './ItemRow';
 
@@ -7,11 +7,20 @@ const headerItems = [
 ];
 
 function OrdersItemsTable() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const localCart = JSON.parse(localStorage.getItem('carrinho'));
+    setCart(localCart);
+  }, []);
+
   return (
     <div className="order-items-table">
       <TableHeader headerItems={ headerItems } />
       <div className="items-container">
-        <ItemRow />
+        { cart.map((item, index) => (
+          <ItemRow key={ index } cartItem={ { item, index } } />
+        )) }
       </div>
       <div className="order-total-price">Total: R$ 21,00</div>
     </div>
