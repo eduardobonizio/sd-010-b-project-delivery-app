fe5fc0aea019318e0b0c37b4a3ee40ec127f798f
jaarguello <johnny.arguello007@gmail.com>
2021-11-18 18:41:29 -0300

    adds admin register user new validate logic

--- front-end/src/helper/verifyForm/admin_registerUser.js
+++ front-end/src/helper/verifyForm/admin_registerUser.js
@@ -1,30 +1,43 @@
 // import invokeAlert from '../functions/invokeAlert';
 // import { INVALID_EMAIL_PASSWORD, EMPTY_FIELD, INVALID_NICKNAME_NAME } from '../errorMsgs';
 
+const INVALID_NAME = 'Nome inválido';
+const INVALID_PASSWORD = 'Senha inválido';
+const INVALID_EMAIL = 'E-mail inválido';
+const EMPTY_FIELDS = 'Campos não preenchidos';
+
 export const validateIfFieldsAreFilled = (data) => {
-  const { name, nickname, birthdate, email, password } = data;
-  if (!name || !nickname || !birthdate || !email || !password) {
-    return invokeAlert(EMPTY_FIELD);
+  const { name, email, password, role } = data;
+  if (!name || !role || !email || !password) {
+    return EMPTY_FIELDS;
   }
 
   return true;
 };
 
 export const validateIfFieldsAreCorrect = (data) => {
-  const { name, nickname, email, password } = data;
+  const { name, email, password } = data;
+  console.log(data);
 
-  const mailFormat = /[a-z]+@[a-z]+.com/g;
+  const mailFormat = /[a-z0-9]+@[a-z]+.com/g;
   const less = 6;
-  const nameLess = 6;
-  validateIfFieldsAreFilled(data);
-
-  if (nickname.length < less || name < nameLess) {
-    return invokeAlert(
-      INVALID_NICKNAME_NAME,
-    );
+  const nameLess = 12;
+  const errorArr = [];
+  const fields = validateIfFieldsAreFilled(data);
+  if (fields) {
+    errorArr.push(fields);
   }
-  if (!email.match(mailFormat)
-  && password.length < less) return invokeAlert(INVALID_EMAIL_PASSWORD);
+
+  if (name < nameLess) {
+    errorArr.push(INVALID_NAME);
+  }
+  if (password < less) {
+    errorArr.push(INVALID_PASSWORD);
+  }
+  if (!email.match(mailFormat)) {
+    errorArr.push(INVALID_EMAIL);
+  }
+  if (errorArr[0] !== true) return errorArr;
 
   return true;
 };
