e0e1bf8a77c0c903e65bc4abbbc07bcf0533936c
Roger Franco <rogerfrancogalt@gmail.com>
2021-11-18 17:24:24 -0400

    testando socket

--- front-end/src/pages/OrderDetail.js
+++ front-end/src/pages/OrderDetail.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header'
+import { Link } from 'react-router-dom';
+import socket from 'socket.io-client'
+
 
 export default function OrderDetail() {
+  const [status, setStatus] = useState()
+
+  const currentDate = (date) => {
+    const data = new Date(date);
+    const dia = data.getDate();
+    const mes = (data.getMonth() + 1);
+    const ano = data.getFullYear();
+    return `${dia}/${mes}/${ano}`;
+  };
+  
+  socket.on()
+  useEffect(() => {
+  }, [])
+
   return (
-    <div />
+    <div>
+      <Header />
+      <Link to={'acesso à tela de detalhes'}>
+        <span
+          data-testid={ `customer_products__card-orderId-${id}`  }
+        >
+          (id do pedido)
+        </span>
+        <span
+          data-testid={ `customer_products__card-deliveryStatus-${id}` }
+        >
+          status(pendente/entregue/preparado)
+        </span>
+        <span
+          data-testid={ `customer_products__element-order-date-${id}` }
+        >
+          {currentDate}
+        </span>
+        <span
+          data-testid={ `customer_products__card-price-${id}`}
+        >
+          (preço total)
+        </span>
+      </Link>
+    </div>
   );
 }
