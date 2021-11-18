05df26059c733a82956a3eb4daa1a56f3b62ae1f
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-18 09:39:34 -0300

    refactor(SellersSelect): Fetch em /users/sellers
    - removendo propTypes
    - o componente faz a requisição no back-end para obter os vendedores
    - o array de vendedores é guardado no estado local do componente
    - renderiza com o nome e id que vem do DB

--- front-end/src/components/Checkout/SellersSelect.jsx
+++ front-end/src/components/Checkout/SellersSelect.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import dataTestIdDict from '../../utils/dataTestIdDict';
 
 const { dataTestId29 } = dataTestIdDict;
 
-function SellersSelect({ sellers }) {
+function SellersSelect() {
+  const [sellers, setSellers] = useState([]);
+
+  const getAllSellers = async () => {
+    const endPoint = 'http://localhost:3001/users/sellers';
+    try {
+      const { data } = await axios.get(endPoint);
+      setSellers(data);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  useEffect(() => {
+    getAllSellers();
+  }, []);
+
   return (
     <label htmlFor="sellers" className="order-address-details-field">
       P. Vendedora Responsável
       <select name="sellers" id="sellers" className="p-10" data-testid={ dataTestId29 }>
-        {sellers.map((seller) => (
-          <option key={ seller } value="fulana">{seller}</option>
+        {sellers.map(({ id, name }) => (
+          <option key={ id } value={ id }>{name}</option>
         ))}
       </select>
     </label>
   );
 }
-
-SellersSelect.propTypes = {
-  sellers: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
 
 export default SellersSelect;
