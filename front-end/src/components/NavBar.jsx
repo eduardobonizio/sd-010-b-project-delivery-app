cc79571eed27721eb97dc49284b45c6d6dcce0b2
Lara-Capila <laracapilak@gmail.com>
2021-11-18 17:42:04 -0300

    style/fix: implementa biblioteca antd no navbar

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,39 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-const NavBar = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link
+import { Layout, Menu } from 'antd';
+
+function NavBar() {
+  const { Header } = Layout;
+  return (
+    <Header style={ { position: 'fixed', zIndex: 1, width: '100%' } }>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={ ['2'] }>
+        <Menu.Item
+          key="produtos"
           data-testid="customer_products__element-navbar-link-products"
-          to="/customer/products"
         >
           PRODUTOS
-        </Link>
-      </li>
-      <li>
-        <Link
-          data-testid="customer_products__element-navbar-link-order"
-          to="customer/orders"
+
+        </Menu.Item>
+        <Menu.Item
+          key="pedidos"
+          data-testid="customer_products__element-navbar-link-orders"
         >
           MEUS PEDIDOS
-        </Link>
-      </li>
-      <li data-testid="customer_products__element-navbar-user-full-name">
-        USERNAME
-      </li>
-      <li>
-        <Link
+
+        </Menu.Item>
+        <Menu.Item
+          key="usuario"
+          data-testid="customer_products__element-navbar-user-full-name"
+        >
+          USERNAME
+
+        </Menu.Item>
+        <Menu.Item
+          key="logout"
           data-testid="customer_products__element-navbar-link-logout"
-          to="/login"
         >
           SAIR
-        </Link>
-      </li>
-    </ul>
-  </nav>
 
-);
-
+        </Menu.Item>
+      </Menu>
+    </Header>
+  );
+}
 export default NavBar;
