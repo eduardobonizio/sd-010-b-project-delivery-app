50c2c4e01f8d3ee2324245aad22ece2700d7ca44
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-11-18 15:25:40 -0300

    fix route page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
-import RegisterForm from './components/RegisterForm';
 import Home from './pages/Home';
 import MyOrders from './pages/MyOrders';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/comum/meuspedidos" element={ <MyOrders /> } />
       <Route exact path="/login" element={ <LoginPage /> } />
-      <Route exact path="/register" element={ <RegisterForm /> } />
+      <Route exact path="/register" element={ <Register /> } />
       <Route exact path="/" element={ <Home /> } />
     </Routes>
   );
