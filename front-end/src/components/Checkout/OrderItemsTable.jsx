b6130a64669aee37132e37c5b7aa6e05fbd96567
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-18 10:37:01 -0300

    feat(customer checkout): adicionando a função removeItem
    - gera um novo array sem o item e atualiza no estado local do componente OrdersItemsTable
    - atualiza o array modificado no localStorage
    - passando a função removeItem para o componente ItemRow através de props

--- front-end/src/components/Checkout/OrderItemsTable.jsx
+++ front-end/src/components/Checkout/OrderItemsTable.jsx
@@ -14,12 +14,21 @@ function OrdersItemsTable() {
     setCart(localCart);
   }, []);
 
+  const removeItem = (id) => {
+    const newCart = cart.filter((item) => item.id !== id);
+    localStorage.setItem('carrinho', JSON.stringify(newCart));
+    setCart(newCart);
+  };
+
   return (
     <div className="order-items-table">
       <TableHeader headerItems={ headerItems } />
       <div className="items-container">
         { cart.map((item, index) => (
-          <ItemRow key={ index } cartItem={ { item, index } } />
+          <ItemRow
+            key={ index }
+            cartItem={ { item, index, functions: { removeItem } } }
+          />
         )) }
       </div>
       <div className="order-total-price">Total: R$ 21,00</div>
