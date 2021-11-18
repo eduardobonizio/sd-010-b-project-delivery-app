27afa7cf8db1344aae97332023db8a1084dadc39
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-18 19:12:15 -0300

    estilização da tabela de detalhes

--- front-end/src/styles/Table.js
+++ front-end/src/styles/Table.js
@@ -4,6 +4,8 @@ export const table = styled.table`
   width: 90%;
   margin: 10px auto;
   border: 1px solid #ccc;
+  text-align: center;
+  border-collapse: collapse;
 `;
 
 export const tr = styled.tr`
@@ -11,10 +13,20 @@ export const tr = styled.tr`
 `;
 
 export const th = styled.th`
-  font-size: 1em;
+  background-color: white;
 `;
 
 export const thead = styled.thead`
   width: 100%;
-  text-align: center;
+  font-size: 1.3em;
+`;
+
+export const tbody = styled.tbody`
+  width: 100%;
+  font-size: 1.1em;
+`;
+
+export const td = styled.td`
+  background-color: #aaa;
+  border: 1px solid black;
 `;
