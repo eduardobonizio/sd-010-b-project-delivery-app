917832aab5148cf44fe1f6a67112221ee93d5e39
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-17 22:58:33 -0300

    restruturação do objeto

--- back-end/src/database/models/User.js
+++ back-end/src/database/models/User.js
@@ -1,15 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User',{
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING,
-  },
-  {
-    timestamps: false,
-    tableName: 'users',
-    // underscored: false,
-  });
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    { timestamps: false, tableName: 'users', /* underscored: false */ }
+  );
 
   User.associate = (models) => {
     User.hasMany(models.Sale, {foreignKey: 'user_id', as: 'sales'});
