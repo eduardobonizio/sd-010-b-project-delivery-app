bb682b4c8eac6f4734f5c54166d205d34c1cc1bf
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-18 01:37:05 -0300

    build - retirando consoles logs

--- back-end/src/app/controllers/productController.js
+++ back-end/src/app/controllers/productController.js
@@ -2,9 +2,7 @@ const rescue = require('express-rescue');
 const productService = require('../services/productService');
 
 const findAll = rescue(async (_req, res) => {
-  console.log('entrei');
   const findUsers = await productService.findAll();
-  console.log('entrei no Controller: ', findUsers);
   return res.status(200).json(findUsers);
 });
 
