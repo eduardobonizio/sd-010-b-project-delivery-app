72a4176a5a3a7d279ef809f361d75e471e450a75
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-18 22:47:46 -0300

    refactor(saleController): controller retornando saleId
    - o controller envia como resposta o saleId da função create no service

--- back-end/src/api/Controllers/saleController.js
+++ back-end/src/api/Controllers/saleController.js
@@ -3,8 +3,8 @@ const saleService = require('../Services/saleService');
 const create = async (req, res) => {
   const { sale } = req.body;
   try {
-    await saleService.create(sale);
-    return res.status(201).end();
+    const saleId = await saleService.create(sale);
+    return res.status(201).json({ saleId });
   } catch (error) {
     console.log(error.message);
     return res.status(500).send('Erro interno');
