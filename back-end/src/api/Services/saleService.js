2b9871ff90385ad671391e34c105118d677d3ee3
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-18 22:44:27 -0300

    refactor(saleService): inserindo dados na tabela sales_product
    - a função create no service de sales, realiza uma transação que insere os dados nas tabelas sales e sales_product

--- back-end/src/api/Services/saleService.js
+++ back-end/src/api/Services/saleService.js
@@ -1,11 +1,40 @@
-const { sale } = require('../../database/models');
+const Sequelize = require('sequelize');
+const { sale, salesProducts } = require('../../database/models');
+
+const env = process.env.NODE_ENV || 'test';
+const config = require('../../database/config/config')[env];
+
+const sequelize = new Sequelize(config);
+
+const mapSalesProducts = (products, saleId) => 
+  products.map(({ quantity, id }) => ({ productId: id, saleId, quantity }));
+
+const createSaleTransaction = async (payload) => sequelize.transaction(async (t) => {
+    const {
+      userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status, products,
+    } = payload;
+
+    const { id: saleId } = await sale.create({
+      userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status,
+    }, { transaction: t });
+
+    const salesProductsData = mapSalesProducts(products, saleId);
+
+    await salesProducts.bulkCreate(salesProductsData, { transaction: t });
+    return saleId;
+  });
 
 const create = async (salePayload) => {
-  const {
-    userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status, 
-  } = salePayload;
-
-  return sale.create({ userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status });
+  try {
+    const result = await createSaleTransaction(salePayload);
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error.message);
+    // return 'deu ruim';
+    // throw new Error(error.message);
+    return error.message;
+  }
 };
 
 module.exports = {
