b95deee1fdc00b476018e508cd0d835c31fb8ab0
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-19 01:12:47 -0300

    aprovando seed

--- back-end/src/database/seeders/20211109215510-Sale.js
+++ back-end/src/database/seeders/20211109215510-Sale.js
@@ -13,6 +13,24 @@ module.exports = {
         delivery_number: '375',
         sale_date: new Date('2021-11-09T19:58:00.000Z'),
         status: 'Entregue'
+      }, {
+        id: 2,
+        user_id: 1,
+        seller_id: 3,
+        total_price: 7.5,
+        delivery_address: 'Av. João da Silva, Bairro - Industrial, Cidade - Rio Branco',
+        delivery_number: '375',
+        sale_date: new Date('2021-11-09T19:58:00.000Z'),
+        status: 'Entregue'
+      }, {
+        id: 3,
+        user_id: 1,
+        seller_id: 3,
+        total_price: 7.5,
+        delivery_address: 'Av. João da Silva, Bairro - Industrial, Cidade - Rio Branco',
+        delivery_number: '375',
+        sale_date: new Date('2021-11-09T19:58:00.000Z'),
+        status: 'Entregue'
       }
     ])
   },
