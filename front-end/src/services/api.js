bbb74f65afd05570e795f15f641876a4a60790e8
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-11-19 10:27:27 -0300

    Mergeando branchs

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -17,14 +17,15 @@ export const getProducts = async () => axios.get('http://localhost:3001/products
   .then(({ data }) => data)
   .catch((err) => err.response);
 
-<<<<<<< HEAD
-export const getSellers = async () => axios.get('http://localhost:3001/user/sellers', token())
+  export const getSellers = async () => axios.get('http://localhost:3001/user/sellers', token())
   .then(({ data }) => data)
   .catch((err) => err.response);
 
 export const checkoutProducts = async (body) => axios.post('http://localhost:3001/sales', body, token())
-=======
-export const getOrders = async () => axios.get('http://localhost:3001/sales', token())
->>>>>>> bfc71c52db114fb9dc5dac425ff0595f71958b8a
   .then(({ data }) => data)
   .catch((err) => err.response);
+
+export const getOrders = async () => axios.get('http://localhost:3001/sales', token())
+  .then(({ data }) => data)
+  .catch((err) => err.response);
+
