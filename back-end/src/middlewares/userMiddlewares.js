0d929d3cf0da280fd0239f506b53b5fc3b53a25e
Jessie <miss_sc_jessi@hotmail.com>
2021-11-18 21:04:19 -0300

    retirado as mensagens de erro do status 409 - back end

--- back-end/src/middlewares/userMiddlewares.js
+++ back-end/src/middlewares/userMiddlewares.js
@@ -2,20 +2,19 @@ const rescue = require('express-rescue');
 const { getUserByEmail, getUserByName } = require('../services/users');
 
 // Comments: Lista de erros
-const errors = {
-  nameAlreadyExistis: 'Nome já existe',
-  emailAlreadyExistis: 'Email já existe',
-};
+// const errors = {
+//   nameAlreadyExistis: 'Nome já existe',
+//   emailAlreadyExistis: 'Email já existe',
+// };
 
 const existsUser = rescue(async (req, res, next) => {
   const { name, email } = req.body;
 
   const findUserName = await getUserByName(name);
-  if (findUserName) return res.status(409).json({ message: errors.nameAlreadyExistis });
+  if (findUserName) return res.status(409).end();
 
   const findUserEmail = await getUserByEmail(email);
-  console.log(findUserEmail);
-  if (findUserEmail) return res.status(409).json({ message: errors.emailAlreadyExistis });
+  if (findUserEmail) return res.status(409).end();
   return next();
 });
 
