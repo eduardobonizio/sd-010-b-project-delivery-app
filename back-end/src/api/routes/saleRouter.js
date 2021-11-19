9137aeb1f1ad7161f728a419e48569dc9471d84c
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-19 19:02:55 -0300

    começa a criacao do endpoint que retorna a informaçao dos produtos baseado no id da venda

--- back-end/src/api/routes/saleRouter.js
+++ back-end/src/api/routes/saleRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { create } = require('../controllers/saleController');
-const { getUserOrders, getSaleInfo } = require('../controllers/saleController');
+const { getUserOrders, getSaleInfo, getProductsOfSale } = require('../controllers/saleController');
 
 const router = express.Router();
 
 router.route('/').post(create);
 router.route('/user').get(getUserOrders);
 router.route('/:id').get(getSaleInfo);
+router.route('/products/:id').get(getProductsOfSale);
 
 module.exports = router;
