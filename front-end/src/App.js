30d761b583efe398a696027b3b0ca53b90c36b61
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-20 00:14:08 -0300

    feature: passa a usar componente de rota privada

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { Switch, Route, Redirect, useHistory } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Products from './pages/Products';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
-  const user = localStorage.getItem('user');
-  const history = useHistory();
-  if (!user) {
-    if (history.location.pathname.includes('register')) {
-      history.push('/register');
-    }
-    history.push('/');
-  }
-
   return (
     <Switch>
-      <Route path="/customer/products" component={ Products } />
+      <PrivateRoute path="/customer/products" component={ Products } />
       <Route path="/register" component={ Register } />
       <Route path="/login" component={ Login } />
       <Redirect strict from="/" to="/login" />
