244a3b3a0a2a110a3f2a473c2f3a172b641ba70a
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-20 01:11:41 -0300

    fix(sales migration): arredondamento de totalPrice
    - corrigindo a precisão decimal em totalPrice

--- back-end/src/database/migrations/20211109193531-sales.js
+++ back-end/src/database/migrations/20211109193531-sales.js
@@ -30,7 +30,7 @@ module.exports = {
       },
       total_price: {
         allowNull: false,
-        type: Sequelize.DECIMAL,
+        type: Sequelize.DECIMAL(10, 2),
       },
       delivery_address: {
         allowNull: false,
