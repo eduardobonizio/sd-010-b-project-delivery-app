dd9cdb7b9d4eb7cfb0a5f36b7ed90dbf4b4954ed
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-20 02:46:11 -0300

    feature: Agora o botão do valor total redireciona para o carrinho

--- front-end/src/components/CartTotal.jsx
+++ front-end/src/components/CartTotal.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import './css/CartTotal.css';
 import Proptypes from 'prop-types';
+import { useHistory } from 'react-router';
 
 function CartTotal({ cartTotal }) {
+  const history = useHistory();
+  const redirectToCart = () => history.push('/customer/checkout');
   return (
     <div
       className="floating-total"
     >
       Ver Carrinho: R$
-      <span
+      <button
+        type="button"
         data-testid="customer_products__checkout-bottom-value"
+        onClick={ redirectToCart }
       >
         { ` ${cartTotal.toFixed(2).toString().split('.').join(',')}` }
-      </span>
+      </button>
     </div>
   );
 }
