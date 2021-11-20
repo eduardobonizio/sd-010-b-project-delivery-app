f16116906c8fac6c72ba8fe3d63f5ff29e11f808
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-19 23:28:52 -0300

    feature: redireciona se não estiver com dados no local storage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, useHistory } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Products from './pages/Products';
 
 function App() {
+  const user = localStorage.getItem('user');
+  const history = useHistory();
+  if (!user) {
+    if (history.location.pathname.includes('register')) {
+      history.push('/register');
+    }
+    history.push('/');
+  }
+
   return (
     <Switch>
       <Route path="/customer/products" component={ Products } />
