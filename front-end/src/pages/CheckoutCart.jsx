89a175a1f01e1507bd0690fe4cde2877a6900eaa
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-20 05:43:29 -0300

    feature: checkout mostra os produtos que estão no carrinho

--- front-end/src/pages/CheckoutCart.jsx
+++ front-end/src/pages/CheckoutCart.jsx
@@ -4,37 +4,26 @@ import ProductCard from '../components/ProductCard';
 import CartTotal from '../components/CartTotal';
 import './css/Products.css';
 
-function CheckoutCart() {
+function Products() {
   const { name } = JSON.parse(localStorage.getItem('user'));
-  const [products, setProducts] = useState([]);
   const [cart, setCart] = useState();
   const [cartTotal, setCartTotal] = useState(0);
 
   useEffect(() => {
     const getLocalStorageCartItens = async () => {
       const localCart = JSON.parse(localStorage.getItem('cart'));
-      setProducts(localCart);
+      const olyItensInCart = localCart.filter((e) => e.quantity > 0);
+      setCart(olyItensInCart);
     };
     getLocalStorageCartItens();
   }, []);
 
   useEffect(() => {
-    const emptyCart = {};
-    products.forEach((product) => {
-      emptyCart[product.id] = {
-        price: parseFloat(product.price),
-        quantity: 0,
-      };
-    });
-    setCart(emptyCart);
-  }, [products]);
-
-  useEffect(() => {
     let sum = 0;
     localStorage.setItem('cart', JSON.stringify(cart));
     if (cart) {
-      Object.entries(cart).forEach((element) => {
-        sum += element[1].price * element[1].quantity;
+      cart.forEach((element) => {
+        sum += element.price * element.quantity;
       });
     }
     setCartTotal(sum);
@@ -42,21 +31,23 @@ function CheckoutCart() {
 
   return (
     <>
+      {console.log('cart', cart)}
       <TopBar name={ name } />
       <div className="products-container">
-        { products.map((product) => (<ProductCard
-          key={ product.id }
-          id={ product.id }
-          name={ product.name }
-          price={ product.price }
-          urlImage={ product.urlImage }
-          setCart={ setCart }
-          cart={ cart }
-        />))}
+        {
+          cart && cart.map((product) => (
+            <ProductCard
+              key={ product.key }
+              product={ product }
+              setCart={ setCart }
+              cart={ cart }
+            />
+          ))
+        }
       </div>
       <CartTotal cartTotal={ cartTotal } />
     </>
   );
 }
 
-export default CheckoutCart;
+export default Products;
