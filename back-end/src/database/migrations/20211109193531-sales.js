2aae0fc16e61c63453b35057cee86958805ec296
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-20 01:13:41 -0300

    fix(sales migration): valor padrão para saleDate
    - corrigindo para o que o valor seja inserido manualmente

--- back-end/src/database/migrations/20211109193531-sales.js
+++ back-end/src/database/migrations/20211109193531-sales.js
@@ -43,7 +43,6 @@ module.exports = {
       sale_date: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: new Date(),
       },
       status: {
         allowNull: false,
