634ee99ad7188446701a4d91ef6d61a315660ce1
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-20 05:03:42 -0300

    refatora: Trata cards como array

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -22,8 +22,8 @@ function Products() {
     let sum = 0;
     localStorage.setItem('cart', JSON.stringify(cart));
     if (cart) {
-      Object.entries(cart).forEach((element) => {
-        sum += element[1].price * element[1].quantity;
+      cart.forEach((element) => {
+        sum += element.price * element.quantity;
       });
     }
     setCartTotal(sum);
@@ -31,12 +31,14 @@ function Products() {
 
   return (
     <>
+      {console.log(Array.isArray(cart))}
       <TopBar name={ name } />
       <div className="products-container">
         {
           cart && cart.map((product) => (
             <ProductCard
               key={ product.key }
+              product={ product }
               setCart={ setCart }
               cart={ cart }
             />
@@ -49,69 +51,3 @@ function Products() {
 }
 
 export default Products;
-
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import TopBar from '../components/TopBar';
-// import ProductCard from '../components/ProductCard';
-// import CartTotal from '../components/CartTotal';
-// import './css/Products.css';
-
-// function Products() {
-//   const { name } = JSON.parse(localStorage.getItem('user'));
-//   const [products, setProducts] = useState([]);
-//   const [cart, setCart] = useState();
-//   const [cartTotal, setCartTotal] = useState(0);
-
-//   useEffect(() => {
-//     const getProducts = async () => {
-//       const response = await axios.get('http://localhost:3001/products');
-//       setProducts(response.data);
-//     };
-//     getProducts();
-//   }, []);
-
-//   useEffect(() => {
-//     const emptyCart = {};
-//     products.forEach((product) => {
-//       emptyCart[product.id] = {
-//         price: parseFloat(product.price),
-//         quantity: 0,
-//         name: product.name,
-//         urlImage: product.urlImage,
-//       };
-//     });
-//     setCart(emptyCart);
-//   }, [products]);
-
-//   useEffect(() => {
-//     let sum = 0;
-//     localStorage.setItem('cart', JSON.stringify(cart));
-//     if (cart) {
-//       Object.entries(cart).forEach((element) => {
-//         sum += element[1].price * element[1].quantity;
-//       });
-//     }
-//     setCartTotal(sum);
-//   }, [cart]);
-
-//   return (
-//     <>
-//       <TopBar name={ name } />
-//       <div className="products-container">
-//         { products.map((product) => (<ProductCard
-//           key={ product.id }
-//           id={ product.id }
-//           name={ product.name }
-//           price={ product.price }
-//           urlImage={ product.urlImage }
-//           setCart={ setCart }
-//           cart={ cart }
-//         />))}
-//       </div>
-//       <CartTotal cartTotal={ cartTotal } />
-//     </>
-//   );
-// }
-
-// export default Products;
