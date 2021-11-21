a7b7a2304ec96c7659081c16fb2a879d0c6056d1
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-20 23:17:52 -0300

    feature: começa a montar a rota de checkout - venda

--- back-end/src/routers/customerRouter.js
+++ back-end/src/routers/customerRouter.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const auth = require('registry-auth-token');
 
 const customerRouter = express.Router();
 
 customerRouter.get('/products');
 
-customerRouter.get('/checkout');
+customerRouter.post('/checkout', async (req, res) => {
+  console.log('Retorna o id do pedido que foi criado no banco de dados');
+  return res.status(200).send({ message: 'under construction' });
+});
 
 customerRouter.get('/orders');
 
-customerRouter.post('/order/:id');
+customerRouter.post('/order/:id', auth, async (req, res) => {
+  console.log('Rota para retorno de uma ordem de compra');
+  return res.status(200).send({ message: 'under construction' });
+});
 
 module.exports = {
   customerRouter,
