1e5ad67c659cffeab9036f6977e7fe5bd109b202
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-22 11:19:56 -0300

    fix: corrige importações do body

--- back-end/src/routers/customerRouter.js
+++ back-end/src/routers/customerRouter.js
@@ -6,8 +6,8 @@ const customerRouter = express.Router();
 customerRouter.get('/products');
 
 customerRouter.post('/checkout', auth, async (req, res) => {
-  const { token, totalPrice, products, address, adressNumber } = req.body;
-  console.log(token, totalPrice, products, address, adressNumber);
+  const { totalPrice, products, address, adressNumber } = req.body;
+  console.log(totalPrice, products, address, adressNumber);
   console.log('Retorna o id do pedido que foi criado no banco de dados');
   return res.status(200).send({ message: 'under construction' });
 });
