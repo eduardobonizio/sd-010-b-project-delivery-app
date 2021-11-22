35f432c60a7c566f991200eec660c0f7e5cd8a35
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-22 13:52:58 -0300

    fix: remove estilização da página checkout para evitar bug do avaliador

--- front-end/src/components/CheckoutProductCard.jsx
+++ front-end/src/components/CheckoutProductCard.jsx
@@ -1,71 +1,11 @@
 import React from 'react';
 import Proptypes from 'prop-types';
-import { Button, Card, Form, InputGroup } from 'react-bootstrap';
-// import './css/ProductCard.css';
+import { Button } from 'react-bootstrap';
 
 function CheckoutProductCard(props) {
-  const { product, product: { id, name, price, urlImage, quantity },
+  const { product, product: { id, name, price, quantity },
     setCart, cart } = props;
   const index = cart.indexOf(product);
-
-  const addOne = (cardId) => {
-    const elementId = `input-${cardId}`;
-    const input = document.getElementById(elementId);
-    const regex = /^[0-9\b]+$/;
-    if (regex.test(input.value)) {
-      input.value = parseInt(input.value, 10) + 1;
-    } else {
-      input.value = 1;
-    }
-    const newCart = [...cart];
-    newCart[index] = {
-      id,
-      price,
-      quantity: parseInt(input.value, 10),
-      name,
-      urlImage,
-    };
-    setCart(newCart);
-  };
-
-  const removeOne = (cardId) => {
-    const elementId = `input-${cardId}`;
-    const input = document.getElementById(elementId);
-    const regex = /^[0-9\b]+$/;
-
-    if ((regex.test(input.value)) && (parseInt(input.value, 10) > 0)) {
-      input.value = parseInt(input.value, 10) - 1;
-    } else {
-      input.value = 0;
-    }
-    const newCart = [...cart];
-    newCart[index] = {
-      id,
-      price,
-      quantity: parseInt(input.value, 10),
-      name,
-      urlImage,
-    };
-    setCart(newCart);
-  };
-
-  const changeCart = (cardId, value) => {
-    let inputValue = value;
-    const idArray = cardId.split('-')[1] - 1;
-    if (value.isNaN) {
-      inputValue = 0;
-    }
-
-    const newCart = [...cart];
-    newCart[idArray] = {
-      id,
-      price,
-      quantity: parseInt(inputValue, 10),
-      name,
-      urlImage,
-    };
-    setCart(newCart);
-  };
 
   const removeItem = (productIndex) => {
     const newCart = [...cart];
@@ -74,87 +14,47 @@ function CheckoutProductCard(props) {
   };
 
   return (
-    <Card style={ { width: '10rem', height: '325px' } }>
-
-      <Card.Body>
-        <Card.Text>
-          <span
-            data-testid={ `customer_checkout__element-order-table-item-number-${index}` }
-          >
-            { index + 1 }
-          </span>
-        </Card.Text>
-        <Card.Title
-          data-testid={ `customer_checkout__element-order-table-name-${index}` }
-          style={ { fontSize: '5px' } }
+    <div>
+      <div
+        data-testid={ `customer_checkout__element-order-table-item-number-${index}` }
+      >
+        { index + 1 }
+      </div>
+      <div
+        data-testid={ `customer_checkout__element-order-table-name-${index}` }
+      >
+        { name }
+      </div>
+      <div
+        data-testid={ `customer_checkout__element-order-table-quantity-${index}` }
+      >
+        { quantity }
+      </div>
+      <div>
+        R$
+        <div
+          data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }
         >
-          { name }
-        </Card.Title>
-        <Card.Title
-          data-testid={ `customer_checkout__element-order-table-quantity-${index}` }
-          style={ { fontSize: '5px' } }
+          { price.toString().split('.').join(',') }
+        </div>
+      </div>
+      <div>
+        R$
+        <span
+          data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }
         >
-          { quantity }
-        </Card.Title>
-        <Card.Text>
-          R$
-          <span
-            data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }
-          >
-            { price.toString().split('.').join(',') }
-          </span>
-        </Card.Text>
-        <Card.Text>
-          R$
-          <span
-            data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }
-          >
-            { (price * quantity).toFixed(2).toString().split('.').join(',') }
-          </span>
-        </Card.Text>
-        <InputGroup className="sm-3">
-          <Button
-            id={ id }
-            data-testid={ `customer_products__button-card-rm-item-${id}` }
-            variant="success"
-            onClick={ (e) => removeOne(e.target.id) }
-          >
-            -
-          </Button>
-          <Form.Control
-            as="input"
-            size="sm"
-            min="0"
-            max="99"
-            id={ `input-${id}` }
-            data-testid={ `cutomer_checkout__element-order-table-quantity-${index}` }
-            onChange={ (e) => {
-              changeCart(e.target.id, e.target.value);
-            } }
-            type="integer"
-            placeholder="0"
-            defaultValue={ quantity }
-          />
-
-          <Button
-            id={ id }
-            data-testid={ `customer_products__button-card-add-item-${id}` }
-            onClick={ (e) => addOne(e.target.id) }
-            variant="success"
-          >
-            +
-          </Button>
-          <Button
-            id={ id }
-            data-testid={ `customer_checkout__element-order-table-remove-${index}` }
-            onClick={ () => removeItem(index) }
-            variant="success"
-          >
-            Remover
-          </Button>
-        </InputGroup>
-      </Card.Body>
-    </Card>
+          { (price * quantity).toFixed(2).toString().split('.').join(',') }
+        </span>
+      </div>
+      <Button
+        id={ id }
+        data-testid={ `customer_checkout__element-order-table-remove-${index}` }
+        onClick={ () => removeItem(index) }
+        variant="success"
+      >
+        Remover
+      </Button>
+    </div>
   );
 }
 
