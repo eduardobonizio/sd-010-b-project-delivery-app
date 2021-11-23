357671af53778e0779f19000efc9e1aa54813274
Thiago Marchini <thiago@msn.com>
2021-11-23 19:22:34 -0300

    Requisiçao do back-end para os requisito

--- back-end/src/controllers/saleController.js
+++ back-end/src/controllers/saleController.js
@@ -4,7 +4,7 @@ const getOrderByPk = async (req, res) => {
   console.log('Controller');
   const { id } = req.params;
   const { id: userId } = req.body.user;
-  const { code, message, order } = saleService.getOrderByPk({ userId, id });
+  const { code, message, order } = await saleService.getOrderByPk({ userId, id });
 
   if (!order) {
     return res.status(code).json({ message });  
