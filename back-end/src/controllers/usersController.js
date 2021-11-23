9efbcb6b5e7c888ebcc713e90b19c1269a3b85b8
Daniel <darkdanielr@gmail.com>
2021-11-23 14:19:21 -0300

    adicionado endpoint para todas ordens do cliente logado 2

--- back-end/src/controllers/usersController.js
+++ back-end/src/controllers/usersController.js
@@ -1,5 +1,5 @@
-const { Sale, User } = require('../database/models');
 const jwt = require('jsonwebtoken');
+const { Sale, User } = require('../database/models');
 
 const getSeller = async (_req, res) => {
   const response = await User.findAll({ where: { role: 'seller' }, attributes: ['name', 'id'] });
@@ -10,8 +10,8 @@ const getOrders = async (req, res) => {
   const token = req.headers.authorization;
   const { email } = jwt.decode(token);
   const { id } = await User.findOne({ where: { email } });
-  const result = await Sale.findAll({ where: { userId: id }});
+  const result = await Sale.findAll({ where: { userId: id } });
   res.status(200).json(result);
-}
+};
 
 module.exports = { getSeller, getOrders };
