537da96f25600d761725fde2e9c51698dcbdc9d9
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-22 22:58:34 -0300

    fix: passa o user pelo body e não pelo req user

--- back-end/src/middlewares/auth.js
+++ back-end/src/middlewares/auth.js
@@ -13,7 +13,7 @@ const auth = async (req, res, next) => {
 
   try {
     const { data } = await isTokenValid(authorization);
-    req.user = data;
+    req.body.user = data;
     next();
   } catch (err) {
     return res.status(UNAUTHORIZED).json({
