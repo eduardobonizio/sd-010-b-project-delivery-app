3672807da0c295be4bec1df63eaf8388ad40a445
marcos <marcosd3wow@gmail.com>
2021-11-23 16:12:32 -0300

    subindo novamente configuraçoes do socketio back e front

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -8,16 +8,24 @@ const path = require('path');
 const customer = require('../routers/Customer');
 const seller = require('../routers/Seller');
 const admin = require('../routers/Admin');
-const http = require('http');
+// const http = require('http');
 const httpServer = http.createServer(app);
 const error = require('../middleware/error');
+const http = require("http").createServer(app);
 
 app.use(express.json());
 app.use(cors());
 
+const io = require("socket.io")(http,{
+  cors: {
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'PUT'],
+  }
+});
+
 app.use('/images', express.static(path.join(__dirname, '..', '..', 'public','images')));
 
-
+require('../sockets/ping')(io);
 // LOGIN
 app.use('/login', login);
 //REGISTER
