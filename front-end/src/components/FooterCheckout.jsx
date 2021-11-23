e5eff8d2c934b7b1dfdd1c8cc5c9fa7fce04ac35
João Victor Pistorio <joao.pistorio@gmail.com>
2021-11-23 17:36:59 -0300

    fix: fix select value seller.id and add data-testid on elements

--- front-end/src/components/FooterCheckout.jsx
+++ front-end/src/components/FooterCheckout.jsx
@@ -12,18 +12,6 @@ function FooterCheckout() {
     deliveryNumber: '',
   });
 
-  // const toke = localStorage.getItem('user');
-  // console.log(toke.tolen);
-
-  // useEffect(() => {
-  //   const getTotal = () => {
-  //     const result = dataOrder.reduce((prev, curren) => prev + Number(curren.total), 0)
-  //       .toFixed(2).replace('.', ',');
-  //     return result;
-  //   };
-  //   setSale({ ...sale, totalPrice: getTotal() });
-  // }, [dataOrder, sale]);
-
   const handleChange = ({ target: { name, value } }) => {
     console.log(value);
     setSale({ ...sale, [name]: value });
@@ -35,65 +23,52 @@ function FooterCheckout() {
       .then((response) => setSellers(response));
   }, []);
 
-  // const handleSubmit = () => {
-  //   const url = 'http://localhost:3000/sales';
-  //   const header = {
-  //     method: 'POST',
-  //     body: JSON.stringify(sale),
-  //     // authorization:
-  //     headers: new Headers({
-  //       'Content-Type': 'application/json',
-  //       Accept: 'application/json',
-  //     }),
-  //   };
-  //   fetch(url, header)
-  //     .then(() => getTasks())
-  //     .catch((err) => console.log('Erro aqui', err));
-  // };
-
   return (
     <div>
       <h3>Detalhes e Endereço para Entrega</h3>
-      <div>
-        <form>
-          <label htmlFor="seller">
-            P. Vendedora Responsável
-            <select
-              name="sellerId"
-              id="seller"
-              onChange={ (e) => handleChange(e) }
-            >
-              { sellers.map((seller, index) => (
-                <option
-                  key={ index }
-                  value={ seller.id }
-                >
-                  { seller.name }
 
-                </option>
-              )) }
-            </select>
-          </label>
-          <label htmlFor="endereco">
-            Endereço
-            <input
-              type="text"
-              name="deliveryAddress"
-              id="endereco"
-              onChange={ (e) => handleChange(e) }
-            />
-          </label>
-          <label htmlFor="numero">
-            Número
-            <input
-              type="text"
-              name="deliveryNumber"
-              id="numero"
-              onChange={ (e) => handleChange(e) }
-            />
-          </label>
-        </form>
-      </div>
+      <form>
+        <label htmlFor="seller">
+          P. Vendedora Responsável
+          <select
+            name="sellerId"
+            id="seller"
+            onChange={ (e) => handleChange(e) }
+            data-testid="customer_checkout__select-seller"
+          >
+            <option value="choice">Escolha o vendedor</option>
+            { sellers.map((seller, index) => (
+              <option
+                key={ index }
+                value={ seller.id }
+              >
+                { seller.name }
+              </option>
+            )) }
+          </select>
+        </label>
+        <label htmlFor="endereco">
+          Endereço
+          <input
+            type="text"
+            name="deliveryAddress"
+            id="endereco"
+            onChange={ (e) => handleChange(e) }
+            data-testid="customer_checkout__input-address"
+          />
+        </label>
+        <label htmlFor="numero">
+          Número
+          <input
+            type="text"
+            name="deliveryNumber"
+            id="numero"
+            onChange={ (e) => handleChange(e) }
+            data-testid="customer_checkout__input-addressNumber"
+          />
+        </label>
+      </form>
+
     </div>
   );
 }
