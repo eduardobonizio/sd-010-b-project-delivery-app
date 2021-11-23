b811fe5436e0b6fbcf188905c8b7d8c17f5881df
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-11-23 17:47:01 -0300

    criando admin page

--- front-end/src/pages/AdminManage.js
+++ front-end/src/pages/AdminManage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import AdminForm from '../components/AdminForm';
 
 function AdminManage() {
   return (
     <div>
-      <h1> Admin </h1>
+      <h1> Cadastrar novo usuário </h1>
+      <AdminForm />
+      <span
+        id="invalid-message"
+        style={ { visibility: 'hidden' } }
+        data-testid="admin_manage__element-invalid-register"
+      />
     </div>
   );
 }
