1add647d6dabb964d137f4f8f33393f26b5fd13f
CIGR2021 <appencigr@gmail.com>
2021-11-23 13:47:11 -0300

    remoção dos componentes do material ui

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -1,65 +1,48 @@
-import * as React from 'react';
-import Container from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React from 'react';
 
-const card = (
-  <Container>
-    <CardContent>
-      <Typography
-        data-testid="customer_products__element-card-price-"
-        variant="h5"
-        component="div"
-      >
-        R$ 0,00
-      </Typography>
-      <Typography
-        data-testid="customer_products__img-card-bg-image-"
-        // sx={ { mb: 1.5 } }
-        color="text.secondary"
-      >
-        Imagem
-      </Typography>
-      <Typography
-        data-testid="customer_products__element-card-title-"
-        // sx={ { fontSize: 14 } }
-        color="text.secondary"
-        gutterBottom
-      >
-        Nome/Título do Produto
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button
-        data-testid="customer_products__button-card-rm-item-"
-        // sx={ { fontSize: 20 } }
-      >
-        -
-      </Button>
-      <input
-        data-testid="customer_products__input-card-quantity-"
-        type="number"
-        title="quantidade de ítens"
-      />
-      <Button
-        data-testid="customer_products__button-card-add-item-"
-        // sx={ { fontSize: 20 } }
-      >
-        +
-      </Button>
-    </CardActions>
-  </Container>
-);
-
-function OutlinedCard() {
+function Card() {
   return (
-    <Container>
-      <Card variant="outlined">{card}</Card>
-    </Container>
+    <main>
+      <section>
+        <h1
+          data-testid="customer_products__element-card-price-"
+        >
+          R$ 0,00
+        </h1>
+        <image
+          data-testid="customer_products__img-card-bg-image-"
+        >
+          Imagem
+        </image>
+        <h3
+          data-testid="customer_products__element-card-title-"
+        >
+          Nome/Título do Produto
+        </h3>
+      </section>
+      <section>
+        <button
+          data-testid="customer_products__button-card-rm-item-"
+          type="button"
+          title="-"
+        >
+          -
+        </button>
+        <input
+          data-testid="customer_products__input-card-quantity-"
+          type="number"
+          title="quantidade de ítens"
+        />
+        <button
+          data-testid="customer_products__button-card-add-item-"
+          type="button"
+          title="+"
+        >
+          +
+        </button>
+      </section>
+    </main>
   );
 }
 
-export default OutlinedCard;
+export default Card;
