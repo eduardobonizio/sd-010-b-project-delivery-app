001558c120fa94be8e1088066180d9af5cf6814b
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-22 22:54:00 -0300

    update: Passa responsabilidade de DB para o service

--- back-end/src/controllers/register.js
+++ back-end/src/controllers/register.js
@@ -1,16 +1,14 @@
-const md5 = require('md5');
 const express = require('express');
-const { User } = require('../models');
 const { newJwtToken } = require('../helpers/jwt');
+const { createUser, findUserByEmail } = require('../services/userService');
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
   const { name, email, password } = req.body;
-  const hashedPassword = md5(password);
 
   try {
-    const user = await User.create({ name, email, password: hashedPassword, role: 'customer' });
+    const user = await createUser(name, email, password);
 
     if (user) {
       const token = await newJwtToken(email, user.id);
@@ -29,7 +27,7 @@ router.post('/', async (req, res) => {
 
 router.post('/check', async (req, res) => {
   const { email } = req.body;
-  const checkExistence = await User.findOne({ where: { email } });
+  const checkExistence = await findUserByEmail(email);
   if (checkExistence.data) return res.status(409);
   return res.status(200);
 });
