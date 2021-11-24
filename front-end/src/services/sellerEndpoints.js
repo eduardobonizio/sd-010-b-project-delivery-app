85a24480338ae93fd96efca76072b275ebe0fd56
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-24 18:19:27 -0300

    adicionando o token

--- front-end/src/services/sellerEndpoints.js
+++ front-end/src/services/sellerEndpoints.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
 const getSaleById = async (id) => {
+  const { token } = JSON.parse(localStorage.getItem('user'));
   const endpoint = `http://localhost:3001/sales/${id}`;
-  const { data } = await axios.get(endpoint);
+  const { data } = await axios.get(endpoint, { headers: { Authorization: token } });
   return data;
 };
 
 const updateSaleStatus = async (id, status) => {
+  const { token } = JSON.parse(localStorage.getItem('user'));
   const endpoint = `http://localhost:3001/sales/${id}`;
   try {
     await axios.patch(
@@ -15,6 +17,7 @@ const updateSaleStatus = async (id, status) => {
       {
         headers: {
           'Content-Type': 'application/json',
+          Authorization: token,
         },
       },
     );
