769c20303fdf5106d509b12a9fd4548c04b35f5b
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-11-24 17:42:27 -0300

    removi comentarios de code antigo

--- front-end/src/pages/CustomerOrders.jsx
+++ front-end/src/pages/CustomerOrders.jsx
@@ -1,4 +1,3 @@
-// import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import CardOrderCustomer from '../components/CardOrderCustomer';
 import Header from '../components/Header';
