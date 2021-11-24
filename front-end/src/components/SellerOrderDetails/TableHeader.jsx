944437126d51415f3b6f7b0a73fba8dd5238bd66
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-24 00:44:10 -0300

    refactor(TableHeader): retirando props e propTypes

--- front-end/src/components/SellerOrderDetails/TableHeader.jsx
+++ front-end/src/components/SellerOrderDetails/TableHeader.jsx
@@ -1,16 +1,12 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 
-function TableHeader({ headerItems }) {
+function TableHeader() {
+  const headerItems = ['Item', 'Descrição', 'Quantidade', 'Valor unitário', 'Sub-total'];
   return (
     <div className="seller-table-header">
       { headerItems.map((value) => <div key={ value }>{value}</div>)}
     </div>
   );
 }
-
-TableHeader.propTypes = {
-  headerItems: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
 
 export default TableHeader;
