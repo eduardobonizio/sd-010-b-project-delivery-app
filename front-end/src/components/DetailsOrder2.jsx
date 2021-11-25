d7b27a220e165002a638f054a8be51cf0d7d6f83
Jefferson <jffrsncrls@gmail.com>
2021-11-25 18:51:17 -0300

    error> data-test

--- front-end/src/components/DetailsOrder2.jsx
+++ front-end/src/components/DetailsOrder2.jsx
@@ -12,65 +12,94 @@ function DetailsOrder() {
 
   const dataTesteIdQuantidade = 'customer_order_details__element-order-table-quantity-';
 
-  const dataTesteIdSubTotal = 'customer_order_details__element-order-table-sub-total';
+  const dataTesteIdSubTotal = 'customer_order_details__element-order-table-sub-total-';
 
   const dataTesteIdPrice = 'customer_order_details__element-order-total-price-';
   const { id, saleDate, status } = detailsOrder;
   const date = new Date(saleDate);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>{id}</th>
-          <th>{name}</th>
-          <th>{date.toLocaleString().split(',', 1)}</th>
-          <th>{status}</th>
-          <th>Marcar Como Entregue</th>
-        </tr>
-      </thead>
-      <thead>
-        <tr>
-          <th>Item</th>
-          <th>Descrição</th>
-          <th>Quantidade</th>
-          <th>Valor Unitário</th>
-          <th>Sub-Total</th>
-        </tr>
-      </thead>
-      <tbody>
-        {productsAPI.map((element, index) => (
-          <tr key={ element.name }>
-            <td
-              data-testid={ `${dataTesteIdItemNumber}${index}` }
-            >
-              {index + 1}
-            </td>
-            <td
-              data-testid={ `${dataTesteIdName}${index}` }
-            >
-              { element.name }
-            </td>
-            <td
-              data-testid={ `${dataTesteIdQuantidade}${index}` }
-            >
-              { element.SalesProduct.quantity }
-            </td>
-            <td
-              data-testid={ `${dataTesteIdPrice}${index}` }
-            >
-              {Number(element.price).toFixed(2).toString().replace('.', ',')}
-            </td>
-            <td
-              data-testid={ `${dataTesteIdSubTotal}${index}` }
-            >
-              {((Number(element.SalesProduct.quantity)
-              * Number(element.price)).toFixed(2).toString().replace('.', ','))}
-            </td>
+    <>
+      <span>
+        <lable
+          htmlFor="input-id"
+          data-testid={ `customer_order_details__element
+          -order-details-label-order-${id}` }
+        >
+          PEDIDO -
+          <selector id="input-id">{id}</selector>
+          <selector />
+        </lable>
+        <lable
+          htmlFor="input-name"
+          data-testid={ `
+          -order-details-label-seller-name${id}` }
+        >
+          P.Venda
+          <selector id="input-name">{name}</selector>
+        </lable>
+        <lable
+          htmlFor="input-date"
+          data-testid={ `customer_order_details__
+          element-order-details-label-order-date${id}` }
+        >
+          <selector id="input-date">{ date.toLocaleString().split(',', 1)}</selector>
+        </lable>
+        <lable
+          htmlFor="input-status"
+          data-testid={ `customer_order_details__element-
+          order-details-label-delivery-status${id}` }
+        >
+          <input id="input-status" value={ status } />
+        </lable>
+        <lable htmlFor="button">
+          <button
+            type="button"
+            id="button"
+            data-testid={ `customer_order_details__button-
+            delivery-check` }
+          >
+            Marcar Como Entregue
+          </button>
+        </lable>
+      </span>
+      <table>
+        <thead>
+          <tr />
+        </thead>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Sub-Total</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {productsAPI.map((element, index) => (
+            <tr key={ element.name }>
+              <td data-testid={ `${dataTesteIdItemNumber}${index}` }>
+                {index + 1}
+              </td>
+              <td data-testid={ `${dataTesteIdName}${index}` }>{element.name}</td>
+              <td data-testid={ `${dataTesteIdQuantidade}${index}` }>
+                {element.SalesProduct.quantity}
+              </td>
+              <td data-testid={ `${dataTesteIdPrice}${index}` }>
+                {Number(element.price).toFixed(2).toString().replace('.', ',')}
+              </td>
+              <td data-testid={ `${dataTesteIdSubTotal}${index}` }>
+                {(Number(element.SalesProduct.quantity) * Number(element.price))
+                  .toFixed(2)
+                  .toString()
+                  .replace('.', ',')}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
--- front-end/src/components/Order.jsx
+++ front-end/src/components/Order.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 
 function Order({ id, status, date, total }) {
+  const newDate = new Date();
+  const newData = (newDate.toISOString(`${date}`).split('T', 1)[0].split('-'));
   const [isRedirect, setIsRedirect] = useState(false);
   const handleClick = () => {
     setIsRedirect(true);
@@ -24,7 +26,7 @@ function Order({ id, status, date, total }) {
         <span
           data-testid={ `customer_orders__element-order-date-${id}` }
         >
-          {date}
+          {`${newData[2]}/${newData[1]}/${newData[0]}`}
         </span>
         <span>
           {total}
