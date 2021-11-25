b258af199a2ee7d11f0dfb9fe574e46fb77391fd
Victor Lopes <victor.cabrera0209@gmail.com>
2021-11-25 15:45:41 -0300

    finaliza do requisito 21 back end

--- back-end/src/database/models/sales_products.js
+++ back-end/src/database/models/sales_products.js
@@ -2,6 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const SalesProducts = sequelize.define('SalesProducts',
     { quantity: DataTypes.INTEGER },
     { 
+      underscored: true,
       timestamps: false,
       tableName: 'sales_products',
     },
@@ -11,14 +12,16 @@ module.exports = (sequelize, DataTypes) => {
     models.Product.belongsToMany(models.Sale, {
       as: 'sales',
       through: SalesProducts,
-      foreignKey: 'product_id',
-      otherKey: 'sale_id',
+      foreignKey: 'productId',
+      otherKey: 'saleId',
+      underscored: true,
     });
     models.Sale.belongsToMany(models.Product, {
       as: 'products',
       through: SalesProducts,
-      foreignKey: 'sale_id',
-      otherKey: 'product_id',
+      foreignKey: 'saleId',
+      otherKey: 'productId',
+      underscored: true,
     });
   };
 
