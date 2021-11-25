d2d78ec63728d9cf8cc7e15b21501487b38733aa
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-24 22:09:07 -0300

    Feat: Cria o body da table details orders

---
+++ front-end/src/components/ordersDetails/BodyTable.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as styles from './styles';
+
+function BodyTable() {
+  return (
+    <>
+      <styles.TdTable />
+      <styles.TdTable />
+      <styles.TdTable />
+      <styles.TdTable />
+      <styles.TdTable />
+    </>
+  );
+}
+
+export default BodyTable;
