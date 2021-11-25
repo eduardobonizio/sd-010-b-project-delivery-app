e34f28401312372a639581ca4aa57c1999735597
Gildeir <gildeirlopes@gmail.com>
2021-11-24 23:30:07 -0300

    feature: requisitos 22, 23, 24 tela de pedidos do cliente

--- front-end/src/pages/Client/MyRequests/index.jsx
+++ front-end/src/pages/Client/MyRequests/index.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import moment from 'moment';
 import { Link } from 'react-router-dom';
 
 import { apiGetAllSales } from '../../../services/salesProducts/apiRequestSalesProduct';
 import Header from '../../../components/navbar';
-import * as style from './style';
+// import * as style from './style';
 
 export default function CustomerSales() {
-  // const [sales, setSales] = useState([
-  //   { pedido: 1, status: 'pendente', data: '01/01/0000', valor: 1000 },
-  // ]);
+  const dateMoment = moment();
+  console.log(dateMoment);
 
   const [sales, setSales] = useState([]);
 
@@ -23,42 +23,49 @@ export default function CustomerSales() {
   }, []);
 
   return (
-    <>
+    <div>
       <Header />
-      <style.DetailsContainer>
-        { sales.length > 1
-          ? sales.map((curr, index) => (
-            <Link key={ `link:${index}` } to={ `${curr.id}` }>
-              <style.DetailsCard key={ `card:${index}` }>
-                <p
-                  key={ `pedido:${index}` }
-                  data-testid={ `customer_orders__element-order-id-${curr.id}` }
-                >
-                  {console.log(curr.status)}
-                  { curr.id }
-                </p>
-                <p
-                  key={ `status:${index}` }
-                  data-testid={ `customer_orders__element-delivery-status-
-                  ${curr.id}` }
-                >
-                  { curr.status }
-                </p>
-                <p
-                  key={ `data:${index}` }
-                  data-testid={ `customer_orders__element-order-date-${curr.id}` }
-                >
-                  { curr.sale_date }
-                </p>
-                <p
-                  key={ `valor:${curr.total_price}` }
-                >
-                  { curr.total_price }
-                </p>
-              </style.DetailsCard>
+      {sales.map(({ id, status, saleDate, totalPrice }) => {
+        const date = moment(saleDate);
+        return (
+          <div key={ id }>
+            {console.log(sales)}
+            <Link key={ `link:${id}` } to={ `${id}` }>
+              <p
+                data-testid={ `customer_orders__element-order-id-${id}` }
+              >
+                {`Pedido:${id}`}
+              </p>
+              <p
+                data-testid={ `customer_orders__element-order-id-${id}` }
+              >
+                {`Pedido:${id}`}
+              </p>
+              <p
+                data-testid={ `customer_orders__element-delivery-status-${id}` }
+              >
+                { status }
+              </p>
+              <p
+                data-testid={ `customer_orders__element-order-date-${id}` }
+              >
+                {console.log(saleDate)}
+                { date.format('DD/MM/YYYY') }
+              </p>
+              <p
+                data-testid={ `customer_orders__element-card-price-${id}` }
+              >
+                {console.log('price', totalPrice.replace('.', ','))}
+                {totalPrice.replace('.', ',')}
+              </p>
+
+              <p
+                data-testid={ `customer_orders__element-card-address-${id}` }
+              />
             </Link>
-          )) : 'Carregando'}
-      </style.DetailsContainer>
-    </>
+          </div>
+        );
+      })}
+    </div>
   );
 }
--- front-end/src/pages/Common/Login/index.jsx
+++ front-end/src/pages/Common/Login/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as style from './style';
 import { apiRequestLogin } from '../../../services/login/apiRequestLogin';
-import { setOnLocalStorage } from '../../../helpers/localStorage';
+import { setOnLocalStorage, getFromLocalStorage } from '../../../helpers/localStorage';
 
 export default function Login() {
   const [password, setPassword] = useState('');
@@ -31,6 +31,14 @@ export default function Login() {
       navigate('../admin/manage', { replace: true });
     }
   };
+
+  useEffect(() => {
+    const localUser = getFromLocalStorage('user');
+    if (localUser) {
+      console.log('oi');
+      navigate('../customer/products', { replace: true });
+    }
+  }, [navigate]);
 
   useEffect(() => {
     function buttonAble() {
