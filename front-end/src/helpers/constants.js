82a86d2928ea5f7222f840488a9353c976d18c64
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-26 18:04:02 -0300

    fix: muda a url de acordo com o ambiente

--- front-end/src/helpers/constants.js
+++ front-end/src/helpers/constants.js
@@ -1,4 +1,6 @@
-const serverUrl = 'http://localhost:3001';
+const serverUrl = process.env.NODE_ENV === 'production'
+  ? process.env.REACT_APP_PROD_API_URL
+  : process.env.REACT_APP_DEV_API_URL;
 // const serverUrl = 'https://grupo-19-delivery-app.herokuapp.com';
 const test = 'teste';
 
