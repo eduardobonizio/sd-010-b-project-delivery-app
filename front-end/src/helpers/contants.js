597f56b1d94170bfd607b11de78f72bc2cf6785d
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-26 14:34:43 -0300

    fix: modifica para buscar o link diretamente no site do server

--- front-end/src/helpers/contants.js
+++ front-end/src/helpers/contants.js
@@ -1,6 +1,4 @@
-const serverUrl = process.env.NODE_ENV === 'development'
-  ? process.env.REACT_APP_DEV_API_URL
-  : process.env.REACT_APP_PROD_API_URL;
+const serverUrl = 'https://grupo-19-delivery-app.herokuapp.com';
 const test = 'teste';
 
 export {
