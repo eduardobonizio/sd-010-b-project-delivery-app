3557061aba62920cce37ccb01197bee53fdd5e05
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-26 17:45:01 -0300

    fix: agora aponta para a pasta build dentro de public

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -21,7 +21,7 @@ const registerController = require('../controllers/register');
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static('public/build'));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginController);
