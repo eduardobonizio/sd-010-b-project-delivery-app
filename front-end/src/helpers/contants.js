1b5c76b8a9af60de7382bec7606b523e62ba959c
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-26 14:30:55 -0300

    fix: tenta inverter a logica para o heroku pegar o link certo do serve

--- front-end/src/helpers/contants.js
+++ front-end/src/helpers/contants.js
@@ -1,6 +1,6 @@
-const serverUrl = process.env.NODE_ENV === 'production'
-  ? process.env.REACT_APP_PROD_API_URL
-  : process.env.REACT_APP_DEV_API_URL;
+const serverUrl = process.env.NODE_ENV === 'development'
+  ? process.env.REACT_APP_DEV_API_URL
+  : process.env.REACT_APP_PROD_API_URL;
 const test = 'teste';
 
 export {
